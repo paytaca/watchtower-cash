94d7939f2186e88c130b97d3f9ccd28eb0ad46cf
Reamon Sumapig <29reamonsumapig@gmail.com>
2020-08-17 11:25:32 +0800

    Issue: Unit Test | Orientation Solution: Initial orientation of test files

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+# import pytest
+
+# from django.core.management import call_command
+
+# @pytest.mark.django_db
+# @pytest.fixture(scope='function')
+# def django_db_setup(django_db_setup, django_db_blocker):
+#     with django_db_blocker.unblock():
+#         call_command('loaddata', 'demo-fixtures.json')
