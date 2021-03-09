a790b8cec92c2858bc85c2b6a9423ffc915a2a5d
Reamon Sumapig <29reamonsumapig@gmail.com>
2021-03-09 11:37:48 +0800

    Note: _converted bchaddress_scanner from being a celery task into an ordinary script for future use_ Issue: #000

--- main/utils/transaction_recovery_tool.py
+++ main/utils/transaction_recovery_tool.py
@@ -5,6 +5,35 @@ import requests, json
 
 class TRT(object):
     
+    def bchaddress_scanner(self, bchaddress):
+        addresses = [bchaddress]
+        source = 'bch-address-scanner'
+        url = 'https://rest.bitcoin.com/v2/address/transactions'
+        data = { "addresses": addresses}
+        resp = requests.post(url, json=data)
+        data = json.loads(resp.text)
+        for row in data:
+            for tr in row['txs']:
+                blockheight, created = BlockHeight.objects.get_or_create(number=tr['blockheight'])
+                for out in tr['vout']:
+                    amount = out['value']
+                    spent_index = tr['vout'][0]['spentIndex'] or 0
+                    if 'addresses' in out['scriptPubKey'].keys():
+                        for legacy in out['scriptPubKey']['addresses']:
+                            address_url = 'https://rest.bitcoin.com/v2/address/details/%s' % legacy
+                            address_response = requests.get(address_url)
+                            address_data = json.loads(address_response.text)
+                            args = (
+                                'bch',
+                                address_data['cashAddress'],
+                                tr['txid'],
+                                out['value'],
+                                source,
+                                blockheight.id,
+                                spent_index
+                            )
+                            if not Transaction.objects.filter(txid=tr['txid']).exists():
+                                save_record(*args)
 
     def recover_bch_tx(self, txid, block=None):
         url = f'https://rest.bitcoin.com/v2/transaction/details/{txn_id}'
