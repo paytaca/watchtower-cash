18655fd9c6393a4135613b2a446b3fdef0016cc6
Reamon Sumapig <29reamonsumapig@gmail.com>
2021-03-11 18:52:42 +0800

    Note: _auto-delete REDIS keys during deployment_ Issue: #000

--- main/apps.py
+++ main/apps.py
@@ -5,3 +5,12 @@ class MainConfig(AppConfig):
     
     def ready(self):
         import main.signals
+        from django.conf import settings
+        REDIS_STORAGE = settings.REDISKV
+        REDIS_STORAGE.delete('ACTIVE-BLOCK')
+        REDIS_STORAGE.delete('ACTIVE-BLOCK-TRANSACTIONS')
+        REDIS_STORAGE.delete('ACTIVE-BLOCK-TRANSACTIONS-CURRENT-INDEX')
+        REDIS_STORAGE.delete('ACTIVE-BLOCK-TRANSACTIONS-INDEX-LIMIT')
+        REDIS_STORAGE.delete('ACTIVE-BLOCK-TRANSACTIONS-COUNT')
+        REDIS_STORAGE.delete('GENESIS')
+        REDIS_STORAGE.delete('READY')
