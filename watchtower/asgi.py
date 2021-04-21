d1f4a3f44d24dbaa98b48093c0c477a768108291
Reamon Sumapig <29reamonsumapig@gmail.com>
2021-04-21 16:23:05 +0800

    Note: _setup websocket for watchtower_ Issue: #000

--- watchtower/asgi.py
+++ watchtower/asgi.py
@@ -9,8 +9,23 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import main.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watchtower.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+  "http": django_asgi_app,
+  "websocket": AllowedHostsOriginValidator(
+    AuthMiddlewareStack(
+        URLRouter(
+          main.routing.websocket_urlpatterns
+        )
+    )
+  )
+})
