e2eac4b4ad6c3c1cfb0b029fd2830c20618f7f63
Reamon Sumapig <29reamonsumapig@gmail.com>
2021-05-17 19:46:57 +0800

    Standardized subscription for webhook and websocket
    Other Changes:
    - Updated routing.py and urls.py to use the same path for webhook and websocket subscription.
    - Removed unnecessary imports in view_test.py
    - Updated test.html to work with the new path for weboscket subscription.

--- main/views/view_test.py
+++ main/views/view_test.py
@@ -1,16 +1,6 @@
 from django.shortcuts import render
-
-
-
-
 from django.views import View
 from django.shortcuts import render
-from django.http import JsonResponse
-from main.models import (
-    Subscription,
-    SlpAddress
-)
-from operator import or_
 
 
 class Test(View):
