68a35abcd6494a86c6c78effc8bb1a592cf1b72c
Reamon Sumapig <29reamonsumapig@gmail.com>
2021-05-18 16:50:25 +0800

    Enhancement in subscriptions
    Other Changes:
    - revised api path for subscriptions.
    - created a script that will monitor recipients.
    - updated webserver.conf to use dophane instead of gunicorn (for websocket purpose)

---
+++ main/utils/recipient.py
@@ -0,0 +1,30 @@
+from main.models import Recipient
+from django.db.models import Q
+
+class Recipient(object):
+
+    def __init__(self, web_url=None, telegram_id=None):
+        self.web_url = web_url
+        self.telegram_id = telegram_id
+
+    def find(self):
+        if self.web_url and self.telegram_id:
+            qs = Recipient.objects.filter(Q(web_url=self.web_url) & Q(telegram_id=self.telegram_id))
+            if not qs.exists(): return 'create'
+        if self.web_url:
+            qs = Recipient.objects.filter(web_url=self.web_url)
+            if not qs.exists(): return 'create'
+        if self.telegram_id:
+            qs = Recipient.objects.filter(telegram_id=self.telegram_id)
+            if not qs.exists(): return 'create'
+        return None
+    
+    def get_or_create(self):
+        status = self.find()
+        if status == 'create':
+            recipient = Recipient()
+            recipient.web_url = self.web_url
+            recipient.telegram_id = self.telegram_id
+            recipient.save()
+            return recipient, True
+        return status, False
