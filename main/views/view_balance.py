9f142b08350db6cd53e5e0199c0a70273107a87f
Reamon Sumapig <29reamonsumapig@gmail.com>
2021-05-24 15:27:32 +0800

    Improvements on check balances using SLP Address
    Other Changes:
    - Also display token names.
    - Returned list of tokens when checking balance in slp address

--- main/views/view_balance.py
+++ main/views/view_balance.py
@@ -8,31 +8,33 @@ from main import serializers
 class Balance(APIView):
     
     def get(self, request, *args, **kwargs):
-        slpaddress = kwargs.get('slpaddress', None)
-        bchaddress = kwargs.get('bchaddress', None)
-        tokenid = kwargs.get('tokenid', None)
+        slpaddress = kwargs.get('slpaddress', '')
+        bchaddress = kwargs.get('bchaddress', '')
+        tokenid = kwargs.get('tokenid', '')
 
         data = { 'valid': False }
         balance = 0
         qs = None
 
-        if slpaddress:
-            if slpaddress.startswith('simpleledger:'):
-                if tokenid:
-                    data['tokenid'] = tokenid
-                    data['address'] = slpaddress
-                    qs = Transaction.objects.filter(
-                        Q(address=data['address']) & Q(spent=False) & Q(token__tokenid=data['tokenid'])
-                    )
-        if bchaddress:
-            if bchaddress.startswith('bitcoincash:'):
-                data['address'] = bchaddress
-                qs = Transaction.objects.filter(Q(address=data['address']) & Q(spent=False))
-        if qs:
+        if slpaddress.startswith('simpleledger:'):
+            data['address'] = slpaddress
+            if tokenid:
+                query = Q(address=data['address']) & Q(spent=False) & Q(token__tokenid=tokenid)
+            else:
+                query =  Q(address=data['address']) & Q(spent=False)
+                
+            qs = Transaction.objects.filter(query)
+            qs_balance = qs.values('token__tokenid','token__name').order_by('token__tokenid').annotate(balance=Sum('amount'))
+            data['balance'] = list(qs_balance)
+            data['valid'] = True        
+        
+        if bchaddress.startswith('bitcoincash:'):
+            data['address'] = bchaddress
+            qs = Transaction.objects.filter(Q(address=data['address']) & Q(spent=False))
             qs_balance = qs.aggregate(balance=Sum('amount'))
             balance = qs_balance['balance']
             data['balance'] = balance
             data['valid'] = True        
-
+        
         return Response(data=data, status=status.HTTP_200_OK)
         
