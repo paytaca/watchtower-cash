0064ab252b99d99d1b983a306af7459384f83b75
joemarct <joemar.ct@gmail.com>
2021-09-15 09:02:17 +0800

    Pagination in wallet history response

--- main/views/view_history.py
+++ main/views/view_history.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from django.db.models import F
 from rest_framework import status
 from main.models import Wallet, WalletHistory
+from django.core.paginator import Paginator
 
 
 class WalletHistoryView(APIView):
@@ -10,11 +11,12 @@ class WalletHistoryView(APIView):
     def get(self, request, *args, **kwargs):
         wallet_hash = kwargs.get('wallethash', None)
         token_id = kwargs.get('tokenid', None)
+        page = kwargs.get('page', 1)
         qs = WalletHistory.objects.filter(wallet__wallet_hash=wallet_hash)
         wallet = Wallet.objects.get(wallet_hash=wallet_hash)
         if wallet.wallet_type == 'slp':
             qs = qs.filter(token__tokenid=token_id)
-            data = qs.annotate(
+            history = qs.annotate(
                 _token=F('token__tokenid')
             ).rename_annotations(
                 _token='token_id'
@@ -29,7 +31,7 @@ class WalletHistoryView(APIView):
                 'date_created'
             )
         elif wallet.wallet_type == 'bch':
-            data = qs.values(
+            history = qs.values(
                 'record_type',
                 'txid',
                 'amount',
@@ -38,4 +40,15 @@ class WalletHistoryView(APIView):
                 'recipients',
                 'date_created'
             )
-        return Response(data=data, status=status.HTTP_200_OK)
+        if wallet.version == 1:
+            return Response(data=history, status=status.HTTP_200_OK)
+        else:
+            pages = Paginator(history, 10)
+            page_obj = pages.page(int(page))
+            data = {
+                'history': page_obj.object_list,
+                'page': page,
+                'num_pages': pages.num_pages,
+                'has_next': page_obj.has_next()
+            }
+            return Response(data=data, status=status.HTTP_200_OK)
