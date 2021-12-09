19106a9e769381322263cb6f8deaff215b4e02ed
joemarct <joemar.ct@gmail.com>
2021-12-09 19:08:04 +0800

    Fixed error in task imports

--- main/tasks.py
+++ main/tasks.py
@@ -11,7 +11,8 @@ from main.models import (
     Subscription,
     Address,
     Wallet,
-    WalletHistory
+    WalletHistory,
+    WalletNftToken
 )
 from celery.exceptions import MaxRetriesExceededError 
 from main.utils import slpdb as slpdb_scanner
@@ -1082,3 +1083,38 @@ def transaction_post_save_task(self, address, txid, blockheight_id=None):
                     senders['bch'],
                     recipients['bch']
                 )
+
+
+@shared_task(queue='celery_rebuild_history')
+def rebuild_wallet_history(wallet_id):
+    addresses = Address.objects.filter(wallet_id=wallet_id)
+    for address in addresses:
+        for transaction in address.transactions.all():
+            history_check = WalletHistory.objects.filter(
+                txid=transaction.txid,
+                recipients__contains=[address.address]
+            )
+            if not history_check.exists():
+                transaction.save()
+
+            if transaction.token.token_type == 65:
+                wallet_nft_token_check = WalletNftToken.objects.filter(
+                    wallet_id=wallet_id,
+                    token=transaction.token
+                )
+                if wallet_nft_token_check.exists():
+                    wallet_nft_token = wallet_nft_token_check.last()
+                    if  wallet_nft_token.date_dispensed is None and transaction.spent:
+                        try:
+                            spending_tx = Transaction.objects.get(txid=transaction.spending_txid)
+                            wallet_nft_token.date_dispensed = spending_tx.date_created
+                        except Transaction.DoesNotExist:
+                            pass
+                else:
+                    wallet_nft_token = WalletNftToken(
+                        wallet_id=wallet_id,
+                        token=transaction.token,
+                        date_acquired=transaction.date_created,
+                        acquisition_transaction=transaction
+                    )
+                    wallet_nft_token.save()
