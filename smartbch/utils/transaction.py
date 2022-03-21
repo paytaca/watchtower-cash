3c104b5033a46debf77969d6d5c419970fd14f95
khirvy019 <khirvy_19@yahoo.com>
2022-03-21 09:51:39 +0800

    refactor save transactions by address function & add docstrings to used functions

--- smartbch/utils/transaction.py
+++ smartbch/utils/transaction.py
@@ -1,4 +1,5 @@
 import decimal
+from web3.datastructures import AttributeDict
 from smartbch.models import Block, Transaction, TokenContract
 
 from .contract import abi
@@ -45,9 +46,8 @@ def save_transaction_transfers(txid):
     erc20_transfers = erc20.events.Transfer().processReceipt(receipt)
     erc721_transfers = erc721.events.Transfer().processReceipt(receipt)
 
-    print(f"erc20: {erc20_transfers}")
-    print(f"erc721: {erc721_transfers}")
-
+    # print(f"erc20: {erc20_transfers}")
+    # print(f"erc721: {erc721_transfers}")
 
     if len(erc20_transfers):
         for event_log in erc20_transfers:
@@ -108,6 +108,85 @@ def save_transaction_transfers(txid):
 
     return instance
 
+def get_transactions_by_address(address, from_block=0, to_block=0, block_partition=0):
+    """
+        Generator function that yields transactions of a given address within a block range
+        Also includes SEP20 & SEP721 Transfer events
+
+    Parameters
+    ------------
+        address: stirng
+            Hex string of wallet address
+        from_block: int
+            Start block to crawl through the blockchain
+        to_block: int
+            End block to crawl through the blockchain
+        block_partition: int
+            Will cause to iterate from start block to end block by N blocks.
+            A means to avoid burst request
+    Yield
+    ------------
+        tx_list: AttributeDict({
+            "from_block": "0x00",
+            "to_block": "0x00",
+            "transactions": [
+                AttributeDict({
+                    "hash": "0x00",
+                    "block_hash": "0x00",
+                    "block_number": 0,
+                    "transaction_index": 0,
+                })
+            ]
+        })
+    """
+    w3 = create_web3_client()
+    if not w3.isAddress(address):
+        return
+
+    # We add option to partition by a number of blocks to have the option to avoid burst of requests
+    block_markers=[from_block, to_block]
+    if block_partition > 0 and block_partition % 1 == 0:
+        block_markers = [*range(from_block, to_block, block_partition), to_block]
+
+    block_markers[0] -= 1
+    for i in range(len(block_markers)-1):
+        tx_map = {}
+        start_block = format_block_number(block_markers[i-1]+1)
+        end_block = format_block_number(block_markers[i])
+        txs = w3.sbch.query_tx_by_addr(
+            address,
+            start_block,
+            end_block,
+            "0x0",
+        )
+        transfer_txs = w3.sbch.query_transfer_events(
+            wallet_address=address,
+            from_block=start_block,
+            to_block=end_block,
+        )
+
+        for tx in txs:
+            tx_map[tx.hash] = AttributeDict({
+                "hash": tx.hash,
+                "block_hash": tx.blockHash,
+                "block_number": int(tx.blockNumber, 16),
+                "transaction_index": int(tx.transactionIndex, 16),
+            })
+
+        for log in transfer_txs:
+            tx_map[log.transactionHash.hex()] = AttributeDict({
+                "hash": log.transactionHash.hex(),
+                "block_hash": log.blockHash.hex(),
+                "block_number": log.blockNumber,
+                "transaction_index": log.transactionIndex,
+            })
+
+        yield AttributeDict({
+            "from_block": start_block,
+            "to_block": end_block,
+            "transactions": [*tx_map.values()],
+        })
+
 
 def save_transactions_by_address(address, from_block=0, to_block=0, block_partition=0):
     """Save transactions of a given address
@@ -130,38 +209,17 @@ def save_transactions_by_address(address, from_block=0, to_block=0, block_partit
             List of smartbch.models.Transaction instances saved to db
             Transactions that existed already are not included here
     """
-    w3 = create_web3_client()
-    if not w3.isAddress(address):
-        return
-
-    # We add option to partition by a number of blocks to have the option to avoid burst of requests
-    block_markers=[from_block, to_block]
-    if block_partition > 0 and block_partition % 1 == 0:
-        block_markers = [*range(from_block, to_block, block_partition), to_block]
-
-    block_markers[0] -= 1
-
     saved_transactions = []
-    for i in range(len(block_markers)-1):
+    iterator = get_transactions_by_address(
+        address,
+        from_block=from_block,
+        to_block=to_block,
+        block_partition=block_partition
+    )
+    for tx_list in iterator:
         unique_txids = set()
-        txs = w3.sbch.query_tx_by_addr(
-            address,
-            format_block_number(block_markers[i-1]+1),
-            format_block_number(block_markers[i]),
-            "0x0",
-        )
-        transfer_txs = w3.sbch.query_transfer_events(
-            wallet_address=address,
-            from_block=format_block_number(block_markers[i-1]+1),
-            to_block=format_block_number(block_markers[i]),
-        )
-
-        # get unique transaction ids of transaction and transfer events 
-        for tx in txs:
+        for tx in tx_list.transactions:
             unique_txids.add(tx.hash)
-
-        for log in transfer_txs:
-            unique_txids.add(log.transactionHash.hex())
 
         # save transactions that are not yet in db
         saved_txs_queryset = Transaction.objects.filter(txid__in=unique_txids)
--- smartbch/utils/web3.py
+++ smartbch/utils/web3.py
@@ -31,6 +31,26 @@ class SmartBCHModule(web3.module.Module):
     # For more details, see RPC docs
     # https://docs.smartbch.org/smartbch/developers-guide/jsonrpc#sbch
 
+    """
+        Returns:
+            txs: list of transactions, the class type of some properties differ from web3's:
+            AttributeDict({
+                'blockHash': '0x6a6509625946f46727be6df14f9e1731666260fe4931040114266e9aa4cf4ae6',
+                'blockNumber': '0x8af41',
+                'from': '0xd56e9af0243ffbbe4c6a801ad8f20a097d247122',
+                'gas': '0x65b6',
+                'gasPrice': '0x3e95ba80',
+                'hash': '0x106afbfde9e02f8cd21d8b29568a43a753460d019b4a672822c129281524369f',
+                'input': '0x',
+                'nonce': '0x78c',
+                'to': '0xda34ad1848424cca7e0ff55c7ef6c6fe46833456',
+                'transactionIndex': '0x0',
+                'value': '0x1c6bf52634000',
+                'v': '0x43',
+                'r': '0xe6ea923b481d48c5c181c6311b38a115bd73cd300cb167e15c34f5d0cca1fc6d',
+                's': '0x14221e625532f8d705eea31654f0076a92744d679181497babd86000ff96885'
+            })
+    """
     query_tx_by_addr = web3.method.Method(
         web3.types.RPCEndpoint("sbch_queryTxByAddr"),
         mungers=[unsafe_munger],
@@ -54,6 +74,25 @@ class SmartBCHModule(web3.module.Module):
         from_block='0x0',
         to_block='latest',
     ):
+        """
+        Returns:
+            logs: list of transaction logs, example of log:
+            AttributeDict({
+                'address': '0x265bD28d79400D55a1665707Fa14A72978FA6043',
+                'topics': [
+                    HexBytes('0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'),
+                    HexBytes('0x000000000000000000000000fea305d5fe76cf6ea4a887234264a256ad269792'),
+                    HexBytes('0x000000000000000000000000659f04f36e90143fcac202d4bc36c699c078fc98')
+                ],
+                'data': '0x0000000000000000000000000000000000000000000000000000000000009c40',
+                'blockNumber': 3632684,
+                'transactionHash': HexBytes('0xfaf08a4f2e8c9e7a456a663976becec744c4b94a92387433822451e2519ba943'),
+                'transactionIndex': 0,
+                'blockHash': HexBytes('0x0853659d93da82f7fd3687efa9ba752cf3280ab4442e9b872e7090e84a6fa47a'),
+                'logIndex': 0,
+                'removed': False
+            })
+        """
         # Apparently, ERC20's & ERC721's `Transfer` event topic is the same hex string
         event_topic = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
         args = {
