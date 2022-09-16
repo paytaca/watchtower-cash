b129005c992bd79f1802071a2de137e0b70b7665
khirvy019 <khirvy_19@yahoo.com>
2022-09-16 09:45:36 +0800

    update hedge positions api's filter options

--- anyhedge/filters.py
+++ anyhedge/filters.py
@@ -1,5 +1,6 @@
 import pytz
 from datetime import datetime
+from django.db import models
 from django_filters import rest_framework as filters
 
 from .models import (
@@ -29,12 +30,49 @@ class LongAccountFilter(filters.FilterSet):
 
 
 class HedgePositionFilter(filters.FilterSet):
+    FUNDING_STATUS_PENDING = 'pending'
+    FUNDING_STATUS_PARTIAL = 'partial'
+    FUNDING_STATUS_READY = 'ready'
+    FUNDING_STATUS_COMPLETE = 'complete'
+    FUNDING_STATUSES = [
+        (FUNDING_STATUS_PENDING, FUNDING_STATUS_PENDING),
+        (FUNDING_STATUS_PARTIAL, FUNDING_STATUS_PARTIAL),
+        (FUNDING_STATUS_READY, FUNDING_STATUS_READY),
+        (FUNDING_STATUS_COMPLETE, FUNDING_STATUS_COMPLETE),
+    ]
+
+    settled = filters.BooleanFilter(
+        field_name='settlement', lookup_expr='isnull', exclude=True,
+        help_text="Boolean filter for settlement state",
+    )
+    funding = filters.ChoiceFilter(
+        method='filter_funding_status', choices=FUNDING_STATUSES,
+        required=False,
+        help_text=" | ".join([e[0] for e in FUNDING_STATUSES]),
+    )
+
     class Meta:
         model = HedgePosition
         fields = [
             "hedge_wallet_hash",
-            "long_wallet_hash"
+            "long_wallet_hash",
+            "settled",
+            "funding",
         ]
+
+    def filter_funding_status(self, queryset, name, value):
+        if value == self.FUNDING_STATUS_PENDING:
+            queryset = queryset.filter(hedge_funding_proposal__isnull=True, long_funding_proposal__isnull=True, funding_tx_hash__isnull=True)
+        elif value == self.FUNDING_STATUS_PARTIAL:
+            queryset = queryset.filter(
+                models.Q(hedge_funding_proposal__isnull=False) | models.Q(long_funding_proposal__isnull=False),
+                funding_tx_hash__isnull=True
+            )
+        elif value == self.FUNDING_STATUS_READY:
+            queryset = queryset.filter(hedge_funding_proposal__isnull=False, long_funding_proposal__isnull=False, funding_tx_hash__isnull=True)
+        elif value == self.FUNDING_STATUS_COMPLETE:
+            queryset = queryset.filter(funding_tx_hash__isnull=False)
+        return queryset
 
 
 class HedgePositionOfferFilter(filters.FilterSet):
