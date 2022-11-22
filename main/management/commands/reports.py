1564455a084245ece2fa9093b654b8b7299faa90
joemarct <joemar.ct@gmail.com>
2022-11-22 12:17:39 +0800

    Per project report on number of wallets and transactions

---
+++ main/management/commands/reports.py
@@ -0,0 +1,60 @@
+from django.core.management.base import BaseCommand, CommandError
+from main.models import Project, Transaction, Wallet
+from django.utils import timezone
+from datetime import timedelta
+
+
+class Command(BaseCommand):
+    help = "Generate reports on a given project"
+
+    def add_arguments(self, parser):
+        parser.add_argument("-p", "--project", type=str, default='paytaca')
+
+    def handle(self, *args, **options):
+        project_name = options['project']
+        project = Project.objects.get(name__iexact=project_name)
+        print('\n########### REPORTS ###########\n')
+        
+        print('WALLETS:\n')
+        qs = Wallet.objects.filter(
+            project=project,
+            wallet_type='bch'
+        )
+        total_wallets = qs.all().count()
+        print('All time: ' + str(total_wallets))
+
+        month_diff = timezone.now() - timedelta(days=30)
+        wallets_month = qs.filter(date_created__date__gte=month_diff).count()
+        daily_average = round(wallets_month / 30)
+        print('Last 30 days: {} (average {} daily)'.format(str(wallets_month), str(daily_average)))
+
+        week_diff = timezone.now() - timedelta(days=7)
+        wallets_week = qs.filter(date_created__date__gte=week_diff).count()
+        daily_average = round(wallets_week / 7)
+        print('Last 7 days: {} (average {} daily)'.format(str(wallets_week), str(daily_average)))
+
+        wallets_today = qs.filter(date_created__date=timezone.now().date()).count()
+        print('Today: ' + str(wallets_today))
+
+        print('\nTRANSACTIONS:\n')
+        qs = Transaction.objects.filter(
+            wallet__project=project
+        )
+
+        total_transactions = qs.order_by('txid').distinct('txid').count()
+        print('All time: ' + str(total_transactions))
+
+        month_diff = timezone.now() - timedelta(days=30)
+        transactions_month = qs.filter(date_created__date__gte=month_diff).count()
+        daily_average = round(transactions_month / 30)
+        print('Last 30 days: {} (average {} daily)'.format(str(transactions_month), str(daily_average)))
+
+        week_diff = timezone.now() - timedelta(days=7)
+        transactions_week = qs.filter(date_created__date__gte=week_diff).count()
+        daily_average = round(transactions_week / 7)
+        print('Last 7 days: {} (average {} daily)'.format(str(transactions_week), str(daily_average)))
+
+        transactions_today = qs.filter(date_created__date=timezone.now().date()).count()
+        print('Today: ' + str(transactions_today))
+
+        print('\n########### END ###########\n')
