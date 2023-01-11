d47f3674f5642c9830d12d98cd95372142e04f55
joemarct <joemar.ct@gmail.com>
2023-01-11 22:09:38 +0800

    Fixed error in migration on an empty database

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 wait-for-it.sh $POSTGRES_HOST:$POSTGRES_PORT
+python /code/manage.py migrate contenttypes
 python /code/manage.py migrate sessions
 python /code/manage.py migrate
 python /code/manage.py collectstatic --noinput
