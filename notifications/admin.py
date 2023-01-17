107892adad6348c982690a61d5dca61efe6ef946
khirvy019 <khirvy_19@yahoo.com>
2023-01-17 15:47:04 +0800

    add list filters for device wallets in admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,8 +1,37 @@
+from django.db import models
 from django.contrib import admin
 
 from .models import DeviceWallet
 
 # Register your models here.
+class HasNoDeviceFilter(admin.SimpleListFilter):
+    title = "has linked device"
+    parameter_name = "has_linked_device"
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1', 'Yes'),
+            ('0', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value() == "1":
+            queryset = queryset.filter(
+                models.Q(gcm_device__isnull=False) | models.Q(apns_device__isnull=False)
+            )
+        elif self.value() == "0":
+            queryset = queryset.filter(
+                gcm_device__isnull=True, apns_device__isnull=True
+            )
+
+        return queryset
+
+
 @admin.register(DeviceWallet)
 class DeviceWalletAdmin(admin.ModelAdmin):
     search_fields = [
@@ -19,5 +48,10 @@ class DeviceWalletAdmin(admin.ModelAdmin):
         "wallet_hash",
         "gcm_device",
         "apns_device",
+        "last_active",
+    ]
+
+    list_filter = [
+        HasNoDeviceFilter,
         "last_active",
     ]
