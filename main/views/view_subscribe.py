a2b90ff18b172d2024c5209f57e1a1074eb6dad3
joemarct <joemar.ct@gmail.com>
2023-01-26 10:52:09 +0800

    Get the appropriate serializer class for address subscription request

--- main/views/view_subscribe.py
+++ main/views/view_subscribe.py
@@ -6,23 +6,26 @@ from rest_framework.permissions import AllowAny
 from rest_framework import generics
 from main import serializers
 
+import logging
+LOGGER = logging.getLogger(__name__)
 
 
 class SubscribeViewSet(generics.GenericAPIView):
     serializer_class = serializers.SubscriberSerializerChatIdentity
     permission_classes = [AllowAny,]
 
+    def get_serializer_class(self, *args, **kwargs):
+        if self.request.data:
+            if self.request.data.get('chat_identity'):
+                return serializers.SubscriberSerializerChatIdentity
+            else:
+                return serializers.SubscriberSerializer
+        else:
+            return serializers.SubscriberSerializerChatIdentity
+
     def post(self, request, format=None):
-        data = None
-        try:
-            serializer = self.get_serializer(data=request.data)
-            if serializer.is_valid():
-                data = serializer.data
-        except KeyError:
-            serializer = serializer.SubscriberSerializer(data=request.data)
-            if serializer.is_valid():
-                data = serializer.data
-        if data:
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
             response = new_subscription(**serializer.data)
             return Response(response, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
