9add94cf676effb786a1b8fb961c89629736df08
SeanNikki123 <sslino@up.edu.ph>
2023-03-15 17:17:12 +0800

    updated ramp admin | created ramp shift view | added ramp shift serializer | added post shift url

--- ramp/urls.py
+++ ramp/urls.py
@@ -1,11 +1,13 @@
 from rest_framework import routers
 from django.urls import re_path
 from .views import (
-    RampWebhookView
+    RampWebhookView,
+    RampShiftView
 )
 
 router = routers.DefaultRouter()
 
 urlpatterns = router.urls + [
-    re_path('webhooks', RampWebhookView.as_view(), name="ramp-webhook")
+    re_path('webhooks', RampWebhookView.as_view(), name="ramp-webhook"),
+    re_path('shift', RampShiftView.as_view(), name="ramp-shift")
 ]
--- ramp/views.py
+++ ramp/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from datetime import datetime
 
 import json
 import requests
 import logging
+
+from .models import (
+    Shift
+)
+from .serializers import (
+    RampShiftSerializer
+)
 
 logger = logging.getLogger(__name__)
 
@@ -17,3 +25,24 @@ class RampWebhookView(APIView):
         
         logger.info(ramp_data)
         return Response({"success": True, "data": ramp_data}, status=200)
+
+class RampShiftView(APIView):
+
+    def post(self, request):
+        data = request.data
+        data['date_shift_created'] = datetime.now()
+        data['date_shift_completed'] = datetime.now()
+
+        serializer = RampShiftSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        logger.info(serializer.data)
+
+        return Response({"success": True}, status=200)
+
+
+
+# class RampShiftHistoryView():
+
+#     def get(self, request):
