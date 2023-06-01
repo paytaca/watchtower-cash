e41e2f0a2263bf279a22f3c3e1d58652f7d7d424
khirvy019 <khirvy_19@yahoo.com>
2023-06-01 11:54:35 +0800

    add api for fetching cashtoken nft groups

--- main/views/view_cashtokens.py
+++ main/views/view_cashtokens.py
@@ -1,9 +1,9 @@
-# from drf_yasg import openapi
-# from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets, mixins
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets, mixins, decorators
 # from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from django.db.models import F, Subquery, OuterRef, Count, Q
+from rest_framework.response import Response
+from django.db.models import OuterRef, Q, Exists
 from django_filters import rest_framework as filters
 # from rest_framework import status
 
@@ -11,6 +11,7 @@ from main.filters import CashNftFilter, TokensViewSetFilter
 from main.models import (
     CashFungibleToken,
     CashNonFungibleToken,
+    Transaction,
     # WalletHistory,
     # Token,
     # WalletNftToken
@@ -51,3 +52,44 @@ class CashNftsViewSet(
         filters.DjangoFilterBackend,
     ]
     filterset_class = CashNftFilter
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        queryset = self.filter_queryset(queryset)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                name="wallet_hash", type=openapi.TYPE_STRING,
+                in_=openapi.IN_QUERY, required=False,
+            ),
+        ]
+    )
+    @decorators.action(detail=False, methods=["get"], filter_backends=[])
+    def groups(self, request, *args, **kwargs):
+        wallet_hash = request.query_params.get("wallet_hash", None)
+        queryset = CashNonFungibleToken.objects.filter_group()
+        if wallet_hash:
+            owned_nfts_subq = CashNonFungibleToken.objects \
+                .annotate_owner_wallet_hash() \
+                .filter(owner_wallet_hash=wallet_hash) \
+                .filter(category=OuterRef("category"))
+
+            queryset = queryset \
+                .filter(Exists(owned_nfts_subq))
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
