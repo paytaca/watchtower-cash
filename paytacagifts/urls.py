8a0ca71f2f0aa3f45f99c2fbbbb65674da7e8112
Reamon Sumapig <29reamonsumapig@gmail.com>
2023-07-07 19:13:55 +0800

    Updated the url to load the template Issue: #N/A

--- paytacagifts/urls.py
+++ paytacagifts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from paytacagifts import views
 from rest_framework_extensions.routers import ExtendedDefaultRouter
@@ -9,4 +9,5 @@ router.register('campaign', views.CampaignViewSet, 'campaign'),
 
 urlpatterns = [
     path('', include(router.urls)),
+    re_path(r"^claim/$", views.GiftClaimView.as_view(),name='claim'),
 ]
