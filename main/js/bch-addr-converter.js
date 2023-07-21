e41f4668241535766db75fe3df53c4d2bf0cb248
Jet Albano <skyjeth1371120@gmail.com>
2023-07-21 14:52:18 +0800

    Supported conversion of p2sh32 addresses

--- main/js/bch-addr-converter.js
+++ main/js/bch-addr-converter.js
@@ -1,19 +1,52 @@
 
 const ADDRESS = process.argv[2];
-const BCH_NETWORK = process.argv[3];
-const CASH_TO_TOKEN_ADDR = process.argv[4];
+const TO_TOKEN_ADDR = process.argv[3];
 
-const {
+import {
+    decodeCashAddress,
     encodeCashAddress,
-    decodeCashAddress
-} = require("@bitauth/libauth");
+    CashAddressType,
+    CashAddressNetworkPrefix,
+} from '@bitauth/libauth';
 
 (() => {
-    const prefix = BCH_NETWORK === 'mainnet' ? 'bitcoincash' : 'bchtest';
-    const type = CASH_TO_TOKEN_ADDR === 'True' ? 2 : 0
-    
+    const isTestnet = ADDRESS.split(':')[0].indexOf('test') >= 0
+    const prefix = isTestnet ? CashAddressNetworkPrefix.testnet : CashAddressNetworkPrefix.mainnet
     const decodedAddr = decodeCashAddress(ADDRESS);
-    const resultAddress = encodeCashAddress(prefix, type, decodedAddr.hash);
+    let type = CashAddressType.p2pkh;
 
+    if (TO_TOKEN_ADDR === 'True') {
+        switch (decodedAddr.type) {
+            case CashAddressType.p2pkh:
+                type = CashAddressType.p2pkhWithTokens;
+                break;
+            case CashAddressType.p2sh:
+                type = CashAddressType.p2shWithTokens;
+                break;
+            case CashAddressType.p2pkhWithTokens:
+                console.log(ADDRESS)
+                return
+            case CashAddressType.p2shWithTokens:
+                console.log(ADDRESS)
+                return
+        }
+    } else {
+        switch (decodedAddr.type) {
+            case CashAddressType.p2pkh:
+                console.log(ADDRESS)
+                return
+            case CashAddressType.p2sh:
+                console.log(ADDRESS)
+                return
+            case CashAddressType.p2pkhWithTokens:
+                type = CashAddressType.p2pkh
+                break
+            case CashAddressType.p2shWithTokens:
+                type = CashAddressType.p2sh
+                break
+        }
+    }
+
+    const resultAddress = encodeCashAddress(prefix, type, decodedAddr.payload);
     console.log(resultAddress);
 })()
