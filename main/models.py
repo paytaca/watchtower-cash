a0889c9b10d819cea196d6f109e405a188d62010
joemarct <joemar.ct@gmail.com>
2023-08-11 11:21:18 +0800

    Refactored unique_constraints in wallet history records

--- main/models.py
+++ main/models.py
@@ -635,12 +635,9 @@ class WalletHistory(PostgresModel):
         verbose_name_plural = 'Wallet histories'
         ordering = ['-tx_timestamp', '-date_created']
         unique_together = [
-            'wallet',
-            'txid',
-            # 'token_index',
-            'token',
-            'cashtoken_ft',
-            'cashtoken_nft',
+            ['wallet', 'txid', 'token'],
+            ['wallet', 'txid', 'cashtoken_ft'],
+            ['wallet', 'txid', 'cashtoken_nft']
         ]
 
     def __str__(self):
