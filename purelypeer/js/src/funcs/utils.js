43e3c9aa6561272cadf41ed3ed00e5aa0568a7b8
Jet Albano <skyjeth1371120@gmail.com>
2023-08-23 16:30:09 +0800

    Refactored generation of merchant vault

--- purelypeer/js/src/funcs/utils.js
+++ purelypeer/js/src/funcs/utils.js
@@ -2,14 +2,8 @@ import BCHJS from '@psf/bch-js'
 const bchjs = new BCHJS()
 
 
-export function toBytes20 (val, encoding = "utf8", toString = false) {
+export function toBytes20 ({ val, encoding = "utf8", toString = false }) {
   let bytes20 = bchjs.Crypto.hash160(Buffer.from(val, encoding))
   if (toString) bytes20 = bytes20.toString("hex")
   return bytes20
-}
-
-export function cashAddrToPubkey ({ address, hash = false, toString = false }) {
-  const legacyAddress = bchjs.Address.toLegacyAddress(address)
-  const pubkey = bchjs.BitcoinCash.decodeBase58Check(legacyAddress)
-  return hash ? toBytes20(pubkey, "utf8", toString) : pubkey
 }
