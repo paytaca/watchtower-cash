76ebd313350c27f261645ceec49a3d2778779fda
ubuntu <rongrammer@hotmail.com>
2023-09-14 09:57:26 +0800

    allow filter by authguard

--- cts/views/utxos.py
+++ cts/views/utxos.py
@@ -79,8 +79,9 @@ class AddressUtxos(APIView):
         return Response(serializer.data)
     
 
-#WIP
 class AuthIdentityOutputs(APIView):
+    serializer_class = UtxoSerializer
+    pagination_class = CustomLimitOffsetPagination
     """
     These API identifies the utxos that are 'IdentityOutput'(s) in the context of BCMR.
     Moreover, in CashToken Studio's case these IdentityOutputs are the genesis tokens that 
@@ -91,19 +92,23 @@ class AuthIdentityOutputs(APIView):
     of a particular AuthGuard also owns the IdentityOutput tokens locked on the AuthGuard.
     """
     @swagger_auto_schema(
-        operation_description="Fetches the identity outputs associated with the address",
+        operation_description="Fetches the identity outputs associated with the (owner's) address",
         responses={status.HTTP_200_OK: UtxoSerializer},
         manual_parameters=[
-            openapi.Parameter('address', openapi.IN_PATH, description="The address that holds/owns the AuthKey (NFT) that can unlock the AuthGuards that stores the IdentityOutputs (Genesis Tokens)", type=openapi.TYPE_STRING),
+            openapi.Parameter('address', openapi.IN_PATH, description="The address that currently owns the AuthKey (NFT) that can unlock the AuthGuard that stores the IdentityOutputs", type=openapi.TYPE_STRING),
+            openapi.Parameter('authguard', openapi.IN_QUERY, description="The Authguard contract's token address. This filters results by Authguard contract's token address. Returns only the IdentityOutputs locked in this Authguard", type=openapi.TYPE_STRING),
+            openapi.Parameter('offset', openapi.IN_QUERY, description="Pagination's offset.", type=openapi.TYPE_STRING),
+            openapi.Parameter('limit', openapi.IN_QUERY, description="Pagination's page limit.Maximum rows per page", type=openapi.TYPE_INTEGER),
         ]
     )
+
     def get(self, request, *args, **kwargs):
         queryset = Transaction.objects.filter(spent=False)
         address  = kwargs.get('address', None)
         if address:
           queryset = queryset.filter(Q(address__address=address) | Q(address__token_address=address))
         
-        queryset = queryset.filter(Q(amount__gt=0) | Q(cashtoken_ft__category__isnull=False) | Q(cashtoken_nft__category__isnull=False))
+        queryset = queryset.filter(Q(cashtoken_ft__category__isnull=False) | Q(cashtoken_nft__category__isnull=False))
         authkeys = queryset.filter(cashtoken_nft__commitment='00') # authkey like
         # We need to get the addresses of the AuthGuard contract
         # associated with each of these AuthKeys.
@@ -125,12 +130,21 @@ class AuthIdentityOutputs(APIView):
 
         for thread in threads:
             thread.join()
-        # paginator = self.pagination_class()
-        # page = paginator.paginate_queryset(queryset, request)
-        # if page is not None:
-        #     serializer = self.serializer_class(page, many=True)
-        #     return paginator.get_paginated_response(serializer.data)
 
-        # serializer = self.serializer_class(queryset, many=True)
-        # return Response(serializer.data)
-        return Response(token_authguard_addresses)
+        authguard_addresses_set = set(map(lambda x: x[list(x.keys())[0]], token_authguard_addresses))
+        identity_outputs = Transaction.objects.filter(
+            Q(index=0), Q(address__token_address__in=authguard_addresses_set) | Q(address__token_address__in=authguard_addresses_set)
+        )
+        
+        if self.request.query_params.get('authguard'):
+            identity_outputs = identity_outputs.filter(address__token_address=self.request.query_params.get('authguard'))
+
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(identity_outputs, request)
+
+        if page is not None:
+            serializer = self.serializer_class(page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+
+        serializer = self.serializer_class(identity_outputs, many=True)
+        return Response(serializer.data)
