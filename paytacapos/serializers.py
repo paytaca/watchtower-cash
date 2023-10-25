e4bb7d25c7cdaa2c57a478f016d19c209eea754f
Jet Albano <skyjeth1371120@gmail.com>
2023-10-25 10:31:50 +0800

    Modified POS payment serializer to use receiving address instead of wallet hash

--- paytacapos/serializers.py
+++ paytacapos/serializers.py
@@ -9,6 +9,7 @@ from django.utils import timezone
 from rest_framework import serializers
 
 from vouchers.serializers import VaultSerializer
+from main.models import Address
 from .models import (
     LinkedDeviceInfo,
     UnlinkDeviceRequest,
@@ -23,6 +24,13 @@ from .utils.websocket import send_device_update
 
 
 REDIS_CLIENT = settings.REDISKV
+
+
+def get_address_or_err(address):
+    address = Address.objects.filter(address=address)
+    if address.exists():
+        return address.first()
+    raise serializers.ValidationError('receiving_address does not exist')
 
 
 class TimestampField(serializers.IntegerField):
@@ -365,19 +373,32 @@ class PosDeviceSerializer(serializers.ModelSerializer):
         return instance
 
 
+class POSDevicePaymentSerializer(serializers.Serializer):
+    posid = serializers.IntegerField(help_text='Resolves to a new posid if negative value')
+    receiving_address = serializers.CharField()
+
+    def validate_posid(self, value):
+        if self.instance and self.instance.posid != value:
+            raise serializers.ValidationError("editing posid is not allowed")
+        return value
+
+
 class POSPaymentSerializer(serializers.Serializer):
     transaction = serializers.CharField()
     otp = serializers.CharField(required=False)
     payment_timestamp = TimestampField()
-    pos_device = PosDeviceSerializer(supress_merchant_info_validations=True)
+    pos_device = POSDevicePaymentSerializer()
 
     def save(self):
         validated_data = self.validated_data
         otp = validated_data.get("otp", None)
         payment_timestamp = validated_data.get("payment_timestamp", timezone.now())
         pos_device_data = validated_data["pos_device"]
-        wallet_hash = pos_device_data["wallet_hash"]
-        posid = pos_device_data["posid"]
+        receiving_address = pos_device_data['receiving_address']
+        posid = pos_device_data['posid']
+
+        address = get_address_or_err(receiving_address)
+        wallet_hash = address.wallet.wallet_hash
 
         response = {
             "success": False,
