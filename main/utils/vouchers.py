bfdf555e992d624084663aee2cde8c3bb5145524
Jet Albano <skyjeth1371120@gmail.com>
2024-01-09 10:07:13 +0800

    Used transaction recipient to differentiate lock and key NFT during websocket updates

--- main/utils/vouchers.py
+++ main/utils/vouchers.py
@@ -1,6 +1,17 @@
-from vouchers.models import Voucher
+from vouchers.models import *
+
+from django.db.models import Q
 
 
-def is_key_nft(category):
+def is_key_nft(category, recipient_address):
     vouchers = Voucher.objects.filter(category=category)
-    return vouchers.exists()
+
+    # we differentiate the lock and key NFT by checking if the recipient is a vault address
+    # as the lock NFT always goes to the vault address and never gets sent anywhere
+    is_vault_recipient = Vault.objects.filter(
+        Q(address=recipient_address) |
+        Q(token_address=recipient_address)
+    )
+
+    is_key = vouchers.exists() and is_vault_recipient
+    return is_key
