de791552704c6dc32334d94a94e2390807931f3e
Jet Albano <skyjeth1371120@gmail.com>
2024-01-18 09:45:12 +0800

    Refactored differentiation condition for lock and key NFTs during websocket data sending

--- main/utils/vouchers.py
+++ main/utils/vouchers.py
@@ -1,17 +1,7 @@
 from vouchers.models import *
 
-from django.db.models import Q
 
-
-def is_key_nft(category, recipient_address):
+def is_key_nft(category, bch_value):
     vouchers = Voucher.objects.filter(category=category)
-
-    # we differentiate the lock and key NFT by checking if the recipient is a vault address
-    # as the lock NFT always goes to the vault address and never gets sent anywhere
-    vault_recipients = Vault.objects.filter(
-        Q(address=recipient_address) |
-        Q(token_address=recipient_address)
-    )
-
-    __is_key_nft = vouchers.exists() and not vault_recipients.exists()
+    __is_key_nft = vouchers.exists() and bch_value == 1000  # lock NFT value will always be > 1000
     return __is_key_nft
