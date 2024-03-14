356fb85807b966b68aec7de4bd8b1580dfa555d4
Marie Edcel Balite <edcel@scibizinformatics.com>
2024-03-14 11:20:57 +0800

    Minor adjustments to market rate model fields

--- rampp2p/models/market_rate.py
+++ rampp2p/models/market_rate.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class MarketRate(models.Model):
     currency = models.CharField(max_length=10, unique=True)
-    price = models.DecimalField(max_digits=18, decimal_places=2, default=0, editable=False)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    price = models.DecimalField(max_digits=18, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
