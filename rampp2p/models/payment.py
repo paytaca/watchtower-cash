8a3da724dcd59c4d854060b2eeecad87768eea10
Marie Edcel Balite <edcel@scibizinformatics.com>
2024-03-18 14:30:44 +0800

    Updated PaymentMethod to allow null account name

--- rampp2p/models/payment.py
+++ rampp2p/models/payment.py
@@ -18,9 +18,9 @@ class PaymentType(models.Model):
     return self.name
 
 class PaymentMethod(models.Model):
-  payment_type = models.ForeignKey(PaymentType, on_delete=models.PROTECT, editable=False)
-  owner = models.ForeignKey(Peer, on_delete=models.CASCADE, editable=False)
-  account_name = models.CharField(max_length=100)
+  payment_type = models.ForeignKey(PaymentType, on_delete=models.PROTECT)
+  owner = models.ForeignKey(Peer, on_delete=models.CASCADE)
+  account_name = models.CharField(max_length=100, blank=True, null=True, default='')
   account_identifier = models.CharField(max_length=100)
   created_at = models.DateTimeField(auto_now_add=True, editable=False)
   modified_at = models.DateTimeField(auto_now=True)
