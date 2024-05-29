116421490025d877704b0fa82c1d067427c16634
Marie Edcel Balite <edcel@scibizinformatics.com>
2024-05-29 12:12:05 +0800

    Fixed inconsistent completion rate returned per ad vs per peer

--- rampp2p/serializers/ad.py
+++ rampp2p/serializers/ad.py
@@ -148,41 +148,30 @@ class AdListSerializer(serializers.ModelSerializer):
         return price
     
     def get_trade_count(self, instance: models.Ad):
-        # Count the number of trades (orders) related to ad owner
-        query = Q(ad_snapshot__ad__owner__id=instance.owner.id)
-        trade_count = models.Order.objects.filter(query).count()
-        return trade_count
+        return models.Order.objects.filter(Q(ad_snapshot__ad__id=instance.id)).count()
 
     def get_completion_rate(self, instance: models.Ad):
         ''' 
         completion_rate = released_count / (released_count + canceled_count + refunded_count)
         '''
-        
-        owner_id = instance.owner.id
-        released_count = self.get_orders_status_count(owner_id, models.StatusType.RELEASED)
-        canceled_count = self.get_orders_status_count(owner_id, models.StatusType.CANCELED)
-        refunded_count = self.get_orders_status_count(owner_id, models.StatusType.REFUNDED)
-        
+        released_count, completed_count = self.get_completed_orders_count(instance.id)
         completion_rate = 0
-        denum = released_count + canceled_count + refunded_count        
-        if denum > 0:
-            completion_rate = released_count / denum * 100
-        
+        if completed_count > 0:
+            completion_rate = released_count / completed_count * 100
         return completion_rate
     
-    def get_orders_status_count(self, owner_id: int, status: models.StatusType):
+    def get_completed_orders_count(self, ad_id: int):
         # Subquery to get the latest status for each order
-        query = Q(order_id=OuterRef('id')) & Q(status=status)
-        latest_status_subquery = models.Status.objects.filter(query).order_by('-created_at').values('id')[:1]
+        latest_status_subquery = models.Status.objects.filter(order_id=OuterRef('id')).order_by('-created_at').values('status')[:1]
         
-        # Retrieve the latest statuses for each order
-        user_orders = models.Order.objects.filter(Q(ad_snapshot__ad__owner__id=owner_id)).annotate(
-            latest_status_id = Subquery(latest_status_subquery)
+        user_orders = models.Order.objects.filter(Q(ad_snapshot__ad__id=ad_id)).annotate(
+            latest_status = Subquery(latest_status_subquery)
         )
 
-        # Filter only the orders with their latest status
-        filtered_orders_count = user_orders.filter(status__id=F('latest_status_id')).count()
-        return filtered_orders_count
+        completed_statuses = [models.StatusType.RELEASED.value, models.StatusType.CANCELED.value, models.StatusType.REFUNDED.value]
+        completed_orders_count = user_orders.filter(status__status__in=completed_statuses).count()
+        release_orders_count = user_orders.filter(status__status=models.StatusType.RELEASED).count()
+        return release_orders_count, completed_orders_count
 
 class AdDetailSerializer(AdListSerializer):
     fees = serializers.SerializerMethodField()
