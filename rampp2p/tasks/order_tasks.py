14840e90159b4ef76e0847747d67f4f25a33193d
Marie Edcel Balite <edcel@scibizinformatics.com>
2024-07-05 13:28:02 +0800

    Fixed periodic task cancel_expired_orders

--- rampp2p/tasks/order_tasks.py
+++ rampp2p/tasks/order_tasks.py
@@ -4,6 +4,7 @@ from rampp2p.models import Status, Order, StatusType
 from rampp2p.serializers import StatusSerializer
 from django.db.models import OuterRef, Subquery
 from django.utils import timezone
+from datetime import timedelta
 from rampp2p.validators import validate_status_inst_count, validate_status_progression
 
 import logging
@@ -11,27 +12,31 @@ logger = logging.getLogger(__name__)
 
 @shared_task(queue='rampp2p__cancel_expired_orders')
 def cancel_expired_orders():
-    # find expired orders with status SUBMITTED and expires_at 
-    last_status = Status.objects.filter(
+    # find expired orders with status SUBMITTED
+    latest_status_subquery = Status.objects.filter(
         order=OuterRef('pk'),
-        status__in=[StatusType.SUBMITTED]
-    ).order_by('-created_at').values('order')[:1]
-    submitted_orders = Order.objects.filter(pk__in=Subquery(last_status))
-    submitted_orders = submitted_orders.filter(expires_at__isnull=False)
+    ).order_by('-created_at').values('status')[:1]
+    annotated_orders = Order.objects.annotate(latest_status = Subquery(latest_status_subquery))
+    submitted_orders = annotated_orders.filter(latest_status=StatusType.SUBMITTED)
+    logger.warn(f'{len(submitted_orders)} submitted orders')
     # cancel expired orders
     for order in submitted_orders:
+        # initialize expires_at if null
+        if order.expires_at is None:
+            order.expires_at = order.created_at + timedelta(hours=24)
+            order.save()
+        # cancel order if expired
         if order.expires_at < timezone.now():
             status = StatusSerializer(data={
                 'status': StatusType.CANCELED,
                 'order': order.id
             })
             try:
-                validate_status_inst_count(status, order.id)
-                validate_status_progression(status, order.id)
+                validate_status_inst_count(StatusType.CANCELED, order.id)
+                validate_status_progression(StatusType.CANCELED, order.id)
                 if status.is_valid():
                     status.save()
             except Exception as err:
                 logger.warn(f'error: {err}')
             
-            order.expires_at = None
             order.save()
