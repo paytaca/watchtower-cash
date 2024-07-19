0671680a90c0734a2bffe60ad3d3b3c5db089f1e
Marie Edcel Balite <edcel@scibizinformatics.com>
2024-07-19 18:32:53 +0800

    Removed unused payment model fields

--- rampp2p/models/payment.py
+++ rampp2p/models/payment.py
@@ -12,21 +12,10 @@ class IdentifierFormat(models.Model):
 class PaymentType(models.Model):
   full_name = models.CharField(max_length=100, db_index=True)
   short_name = models.CharField(max_length=50, default='')
-  formats = models.ManyToManyField(IdentifierFormat)
   notes = models.CharField(max_length=200, null=True, blank=True)
   is_disabled = models.BooleanField(default=False)
-  has_qr_code = models.BooleanField(default=False)
-  acc_name_required = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
   modified_at = models.DateTimeField(auto_now=True)
-
-####### V2 Payments #######
-# class PaymentTypeV2(models.Model):
-#   full_name = models.CharField(max_length=100, db_index=True)
-#   short_name = models.CharField(max_length=50, default='')
-#   notes = models.CharField(max_length=200, null=True, blank=True)
-#   is_disabled = models.BooleanField(default=False)
-#   created_at = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     if self.short_name:
@@ -36,19 +25,10 @@ class PaymentType(models.Model):
 class PaymentMethod(models.Model):
   payment_type = models.ForeignKey(PaymentType, on_delete=models.PROTECT)
   owner = models.ForeignKey(Peer, on_delete=models.CASCADE)
-  account_name = models.CharField(max_length=100, blank=True, null=True, default='')
-  account_identifier = models.CharField(max_length=100)
-  identifier_format = models.ForeignKey(IdentifierFormat, on_delete=models.PROTECT, null=True)
   created_at = models.DateTimeField(auto_now_add=True, editable=False)
-  modified_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return f"{self.id}"
-  
-# class PaymentMethodV2(models.Model):
-#   owner = models.ForeignKey(Peer, on_delete=models.CASCADE)
-#   payment_type = models.ForeignKey(PaymentType, on_delete=models.PROTECT)
-#   created_at = models.DateTimeField(auto_now_add=True, editable=False)
 
 class PaymentTypeField(models.Model):
   fieldname = models.CharField(max_length=100)
