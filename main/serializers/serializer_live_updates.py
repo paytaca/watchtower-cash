a320144f814b78ce5225865c36f42d4f3a52d2a8
Jet Albano <skyjeth1371120@gmail.com>
2024-07-23 14:35:58 +0800

    Updated live payment update types

--- main/serializers/serializer_live_updates.py
+++ main/serializers/serializer_live_updates.py
@@ -6,21 +6,13 @@ from main.models import Address, Subscription
 LIVE_UPDATES_PAYMENT_TYPES = [
     'qr_scanned',
     'cancel_payment',
-    'selecting_vouchers',
-    'sending_vouchers',
-]
-
-vouchers_structure = '''
-total_count: 0,
-total_amount: 0, // BCH
-selected: [ { amount: 0 } ] // BCH
-'''
+    'sending_payment',
+] 
 
 
 class LiveUpdatesPaymentSerializer(serializers.Serializer):
     address = serializers.CharField()
     update_type = serializers.ChoiceField(choices=LIVE_UPDATES_PAYMENT_TYPES)
-    vouchers = serializers.JSONField(required=False, help_text=vouchers_structure)
 
     def validate_address(self, value):
         address = Address.objects.filter(address=value)
