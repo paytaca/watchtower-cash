e4a8cfd195602cd646df87ec797876c93b8e4625
Marie Edcel Balite <edcel@scibizinformatics.com>
2024-09-04 16:53:15 +0800

    Revert "Revert "Code cleanup and refactor user and ad views""
    This reverts commit ae8249a1552583647b14982c4b23b7ddcfb3f45c.

--- rampp2p/views/__init__.py
+++ rampp2p/views/__init__.py
@@ -2,10 +2,12 @@ from .ad import *
 from .appeal import *
 from .contract import *
 from .currency import *
-from .feedback import *
+# from .feedback import *
 from .order import *
 from .payment import *
-from .peer import *
+# from .peer import *
 from .utils import *
-from .arbiter import *
+# from .arbiter import *
 from .webhooks import *
+from .views_user import *
+from .views_ad import *
