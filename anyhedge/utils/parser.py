0fb6df914ec9ab50624c4a9067a5ad66fc211acb
khirvy019 <khirvy_19@yahoo.com>
2024-10-16 16:31:13 +0800

    refactor json parser for anyhedge liquidity provider api

--- anyhedge/utils/parser.py
+++ anyhedge/utils/parser.py
@@ -2,6 +2,18 @@ import re
 import json
 import decimal
 
+def parse_bigint_string(value):
+    if not isinstance(value, str):
+        return value
+
+    match_result = re.match("<bigint\:\s*(\d+)n>", value)
+
+    if not match_result:
+        return value
+
+    number = match_result.group(1)
+    return decimal.Decimal(number)
+
 # https://blag.nullteilerfrei.de/2020/07/11/custom-jsonencoder-and-jsondecoder-to-handle-datetime-in-pythons-json-library/
 class CustomAnyhedgeEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -16,25 +28,14 @@ class CustomAnyhedgeDecoder(json.JSONDecoder):
     def try_bigint(d):
         ret = {}
         for key, value in d.items():
-            if not isinstance(value, str):
-                ret[key] = value
-                continue
-
-            match_result = re.match("<bigint\:\s*(\d+)n>", value)
-
-            if not match_result:
-                ret[key] = value
-                continue
-
-            number = match_result.group(1)
-            ret[key] = decimal.Decimal(number)
-
+            ret[key] = parse_bigint_string(value)
         return ret
 
 class AnyhedgeJSONParser:
     @staticmethod
     def loads(*args, **kwargs):
-        return json.loads(*args, cls=CustomAnyhedgeDecoder, **kwargs)
+        result = json.loads(*args, cls=CustomAnyhedgeDecoder, **kwargs)
+        return parse_bigint_string(result)
 
     @staticmethod
     def dumps(*args, **kwargs):
@@ -45,4 +46,5 @@ class AnyhedgeJSONParser:
         if not isinstance(data, (str, bytes)):
             data = json.dumps(data)
 
+        data = parse_bigint_string(data)
         return cls.loads(data)
