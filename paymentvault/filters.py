9903421dbf50e8d15d3ed935f9fbb9173fd5460e
Jet Albano <skyjeth1371120@gmail.com>
2024-10-24 08:38:04 +0800

    Added merchant pubkey filter for payment vault endpoint

--- paymentvault/filters.py
+++ paymentvault/filters.py
@@ -4,6 +4,14 @@ from .models import PaymentVault
 
 
 class PaymentVaultFilterSet(filters.FilterSet):
+    merchant_pubkey = filters.CharFilter(field_name='merchant__pubkey', lookup_expr='iexact')
+
     class Meta:
         model = PaymentVault
-        fields = '__all__'
+        fields = (
+            'user_pubkey',
+            'merchant',
+            'merchant_pubkey',
+            'address',
+            'token_address',
+        )
