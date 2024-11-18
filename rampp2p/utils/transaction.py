e7ae2ed924db8f3a55a5f443d52a77c62a4a9336
Marie Edcel Balite <edcel@scibizinformatics.com>
2024-11-18 16:20:52 +0800

    Removed unused alternative bchjs fetching of txn

--- rampp2p/utils/transaction.py
+++ rampp2p/utils/transaction.py
@@ -54,10 +54,6 @@ def get_transaction_details(txid):
         if settings.DEBUG:
             if txn is None: 
                 txn = fetch_txn_from_watchtower(txid)
-
-        # # Alternative fetching of transaction if bchn returns None
-        # if txn is None:
-        #     txn = fetch_txn_from_bchjs(txid)
         
         if txn != None:
             response['valid'] = True
@@ -79,54 +75,3 @@ def fetch_txn_from_bchn(txid):
     txn = node.BCH.get_transaction(txid)
     logger.info(f'Fetch txn from bchn: {txn}')
     return txn
-
-class BCHJS:
-    def __init__(self, rest_url, api_token):
-        self.rest_url = rest_url
-        self.api_token = api_token
-
-    def tx_data(self, txid):
-        headers = {'authorization': self.api_token}
-        response = requests.get(f'{self.rest_url}electrumx/tx/data/{txid}', headers=headers)
-        response.raise_for_status()
-        return response.json()
-    
-def fetch_txn_from_bchjs(txid):
-    bchjs = BCHJS(rest_url='https://api.fullstack.cash/v5/', api_token=settings.BCHJS_TOKEN) 
-    try: 
-        raw_txn = bchjs.tx_data(txid) 
-        txn = parse_raw_transaction(bchjs, raw_txn, txid) 
-        logger.info(f'Fetch txn from bchjs: {txn}') 
-        return txn
-    except Exception as error: 
-        logger.exception(error)
-
-def parse_raw_transaction(bchjs, txn, txid):
-    timestamp = txn['details'].get('time')
-    confirmations = txn['details'].get('confirmations')
-    vin = txn['details']['vin']
-    vout = txn['details']['vout']
-
-    # Inputs
-    inputs = []
-    for prev_out in vin:
-        prev_out_tx = bchjs.tx_data(prev_out['txid'])
-        address = prev_out_tx['details']['vout'][prev_out['vout']]['scriptPubKey']['addresses'][0]
-        value = prev_out_tx['details']['vout'][prev_out['vout']]['value']
-        inputs.append({ "address": address, "value": value })
-
-    # Outputs
-    outputs = []
-    for out in vout: 
-        address = out['scriptPubKey']['addresses'][0] 
-        value = out['value'] 
-        outputs.append({ "address": address, "value": value })
-
-    results = { 
-        "txid": txid, 
-        "timestamp": timestamp, 
-        "confirmations": confirmations, 
-        "inputs": inputs, 
-        "outputs": outputs 
-    }
-    return results
