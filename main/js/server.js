3d9d467a1833c499406188c55c0342d19356cdde
ubuntu <rongrammer@hotmail.com>
2024-11-21 15:56:58 +0800

    revert js server, remove signature verify api and mainnet-js package

--- main/js/server.js
+++ main/js/server.js
@@ -5,7 +5,6 @@ import {
     CashAddressType,
     CashAddressNetworkPrefix,
 } from '@bitauth/libauth'
-import { Wallet as MainnetWallet, TestNetWallet } from 'mainnet-js';
 import ElectrumCashProvider from './utils/electrum-cash-provider.js'
 
 
@@ -80,7 +79,6 @@ function getTransactions(address, network=''){
         })
 }
 
-
 const app = express()
 const port = 3000
 
@@ -101,21 +99,6 @@ app.get('/get-transactions/:address', async (req, res) => {
     const response = await getTransactions(req.params.address, network)
     res.send(response)
 })
-
-app.post('/verify-signature', async (req, res) => {
-    try {
-      let Wallet = MainnetWallet
-      if (req.body.bch_address.startsWith('bchtest')) {
-        Wallet = TestNetWallet
-      }
-      let wallet = await Wallet.watchOnly(req.body.bch_address)
-      const verifyResult = await wallet.verify(req.body.message, req.body.signature)  
-      res.send(verifyResult)
-    } catch (error) {
-      console.log('@jsserver /verify-signature: ', error)
-      res.send(error)
-    }
-  })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
