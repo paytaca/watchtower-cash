394a5cc4a2d073a8f32d55d5617b0485f4a085ac
ubuntu <rongrammer@hotmail.com>
2024-11-21 17:06:00 +0800

    adjust model to avoid conf

--- main/models.py
+++ main/models.py
@@ -839,14 +839,14 @@ class AppVersion(models.Model):
     platform = models.CharField(max_length=10, choices=PLATFORM_CHOICES)
     latest_version = models.CharField(max_length=10)
     min_required_version = models.CharField(max_length=10)
-    release_date = models.DateField(default=date.today)
+    release_date = models.DateField(auto_now_add=True)
     notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.platform} - Latest: {self.latest_version}, Min Required: {self.min_required_version}"
-    
-    class Meta:
-        unique_together = ('platform', 'latest_version', 'min_required_version')
+
+
+
 
 
 class WalletAddressApp(models.Model):
