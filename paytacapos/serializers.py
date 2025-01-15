5ccf9fd23af1982f57f94b8c0bb3a0db04417920
Marie Edcel Balite <edcel@scibizinformatics.com>
2025-01-15 12:39:38 +0800

    adjustments to merchant cash out serializers

--- paytacapos/serializers.py
+++ paytacapos/serializers.py
@@ -17,7 +17,7 @@ from .permissions import HasMinPaytacaVersionHeader
 from .utils.broadcast import broadcast_transaction
 from .utils.totp import generate_pos_device_totp
 from .utils.websocket import send_device_update
-
+from rampp2p.serializers import PaymentTypeSerializer
 
 REDIS_CLIENT = settings.REDISKV
 
@@ -848,12 +848,50 @@ class CashOutOrderSerializer(serializers.ModelSerializer):
         model = CashOutOrder
         fields = ('id', 'transactions', 'payment_method', 'status', 'created_at')
 
-class MerchantPaymentMethodSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MerchantPaymentMethod
-        fields = ('id', 'payment_type', 'owner', 'created_at')
-
 class MerchantPaymentMethodFieldSerializer(serializers.ModelSerializer):
+    field_reference = serializers.PrimaryKeyRelatedField(queryset=PaymentTypeField.objects.all(), required=False)
+    payment_method = serializers.PrimaryKeyRelatedField(queryset=PaymentType.objects.all(), required=False)
     class Meta:
         model = MerchantPaymentMethodField
         fields = ('id', 'payment_method', 'field_reference', 'value', 'created_at', 'modified_at')
+
+class MerchantPaymentMethodSerializer(serializers.ModelSerializer):
+    payment_type = PaymentTypeSerializer(read_only=True)
+    owner = MerchantSerializer(read_only=True)
+    payment_fields = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = MerchantPaymentMethod
+        fields = ('id', 'payment_type', 'owner', 'payment_fields', 'created_at')
+
+    def get_payment_fields(self, obj):
+        fields = MerchantPaymentMethodField.objects.filter(payment_method__id=obj.id)
+        serialized_fields = MerchantPaymentMethodFieldSerializer(fields, many=True)
+        return serialized_fields.data
+
+    def create(self, validated_data):
+        payment_type_data = self.initial_data.get('payment_type')
+        owner_data = self.initial_data.get('owner')
+        
+        payment_type = PaymentType.objects.get(id=payment_type_data['id'])
+        owner = Merchant.objects.get(id=owner_data['id'])
+
+        validated_data['payment_type'] = payment_type
+        validated_data['owner'] = owner
+
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        payment_type_data = self.initial_data.get('payment_type')
+        owner_data = self.initial_data.get('owner')
+        
+        if payment_type_data:
+            payment_type = PaymentType.objects.get(id=payment_type_data['id'])
+            instance.payment_type = payment_type
+
+        if owner_data:
+            owner = Merchant.objects.get(id=owner_data['id'])
+            instance.owner = owner
+
+        instance.save()
+        return instance
