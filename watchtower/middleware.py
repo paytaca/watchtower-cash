6f4238b39037d1b58cd19f6c5b82b19ce0005b3d
ubuntu <rongrammer@hotmail.com>
2025-04-16 17:28:02 +0800

    add bip-utils

--- watchtower/middleware.py
+++
@@ -1,9 +0,0 @@
-class CSPMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        # Set Content-Security-Policy header
-        response['Content-Security-Policy'] = "default-src 'self'; connect-src 'self' http://localhost:8000"
-        return response
