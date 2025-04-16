f8c3cdc6197d47cbbd4e3b4c326f5b9995f4c630
ubuntu <rongrammer@hotmail.com>
2025-04-16 14:52:36 +0800

    create join table, enfore wallet uniqueness by index/position of signer

--- multisig/crypto_utils.py
+++ multisig/crypto_utils.py
@@ -1,7 +1,6 @@
-# multisig/crypto_utils.py
-import bip32utils
+from bip_utils import Bip32Secp256k1
 import hashlib
-from coincurve import PublicKey
+from coincurve import PublicKey, PublicKeyXOnly
 from hashlib import sha256
 
 def get_address_index(path: str) -> int:
@@ -12,33 +11,33 @@ def get_address_index(path: str) -> int:
         # Just in case, though address index is usually not hardened
         last = last[:-1]
     
-    return int(last)
+    return last
 
-
-def derive_pubkey_from_xpub(xpub: str, derivation_path: int):
-    """Derives the public key from xpub."""
-    key = bip32utils.BIP32Key.fromExtendedKey(xpub, public=True)
-    address_index = 0
-    if derivation_path:
-      address_index = get_address_index(derivation_path)
-    return key.ChildKey(0).ChildKey(address_index).PublicKey()
+def derive_pubkey_from_xpub(xpub: str, derivation_path: str):
+    """Derives the public key from xpub and returns its hexadecimal representation."""
+    key = Bip32Secp256k1.FromExtendedKey(xpub)
+    public_key = key.DerivePath(get_address_index(derivation_path or '0')).PublicKey()
+    # Return the hexadecimal representation of the public key
+    return public_key.RawCompressed()
 
 def verify_signature(message: str, signature_hex: str, xpub: str, derivation_path: str, algo: str = "ecdsa") -> bool:
     """Verifies the signature using the given xpub, derivation path, and algorithm."""
     try:
-        pubkey_bytes = derive_pubkey_from_xpub(xpub, derivation_path)
-        
-        pubkey = PublicKey(pubkey_bytes)
-        
+        public_key = derive_pubkey_from_xpub(xpub, derivation_path)
         signature = bytes.fromhex(signature_hex)
-        message_bytes = message.encode('utf-8')
-        message_hash = hashlib.sha256()
-        message_hash.update(message_bytes)
-        message_hash = message_hash.digest()
+        message_bytes = message.encode('utf-8')        
+        # message_hash = hashlib.sha256()
+        # message_hash.update(message_bytes)
+        # message_hash = message_hash.digest()
+        
         if not algo or algo == 'ecdsa':
-            return pubkey.verify(signature, message_hash)
+            pubkey = PublicKey(public_key.ToBytes())
+            return pubkey.verify(signature, message_bytes)
         elif algo == "schnorr":
-            return pubkey.schnorr_verify(message_hash, signature)
+            # Pass for now, always sig verification failure using coincurve
+            # pubkey = PublicKeyXOnly(public_key.ToBytes()[1:])
+            # return pubkey.verify(signature, message_hash)
+            raise Exception('Unsupported algorith "schnorr"!')
         else:
             return False
     except Exception as e:
