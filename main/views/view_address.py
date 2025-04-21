46d3fb9fa9351bbb5543323ae2452c4f0be25647
khirvy019 <khirvy_19@yahoo.com>
2025-04-21 09:53:26 +0800

    add filter by wallet hash in address info api

--- main/views/view_address.py
+++ main/views/view_address.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from main.models import Address
 from main.serializers import AddressInfoSerializer
@@ -10,13 +11,31 @@ from notifications.models import DeviceWallet
 class AddressInfoView(APIView):
     serializer_class = AddressInfoSerializer
 
-    @swagger_auto_schema(responses={200: AddressInfoSerializer})
+    @swagger_auto_schema(
+        responses={200: AddressInfoSerializer},
+        manual_parameters=[
+            openapi.Parameter('wallet_hash', openapi.IN_QUERY, type=openapi.TYPE_STRING),
+            openapi.Parameter('project_id', openapi.IN_QUERY, type=openapi.TYPE_STRING),
+        ],
+    )
     def get(self, request, *args, **kwargs):
         bchaddress = kwargs.get('bchaddress', '')
 
+        filter_kwargs = {}
+        query_params = request.query_params
+        if "wallet_hash" in query_params:
+            filter_kwargs["wallet__wallet_hash"] = query_params["wallet_hash"]
+
+        if "project_id" in query_params:
+            filter_kwargs["project__id"] = query_params["project_id"]
+
         address_obj = Address.objects.filter(
-            Q(address=bchaddress) | Q(token_address=bchaddress)
+            Q(address=bchaddress) | Q(token_address=bchaddress),
+            **filter_kwargs,
         ).first()
+
+        if not address_obj:
+            return Response()
 
         wallet = getattr(address_obj, "wallet", None)
         wallet_hash = getattr(wallet, "wallet_hash", "")
