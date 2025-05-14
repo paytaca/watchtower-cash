0d73e37b85221e7f7dfb16edf217116104ba01d3
ubuntu <rongrammer@hotmail.com>
2025-05-14 14:26:35 +0800

    add helper methods

--- multisig/utils.py
+++ multisig/utils.py
@@ -1,7 +1,6 @@
 from bip_utils import Bip32Secp256k1
-import hashlib
-from coincurve import PublicKey, PublicKeyXOnly
-from hashlib import sha256
+from coincurve import PublicKey
+from bip_utils import Bip32Secp256k1, Hash160
 
 def get_address_index(path: str) -> int:
     parts = path.strip().split("/")
@@ -43,3 +42,37 @@ def verify_signature(message: str, signature_hex: str, xpub: str, address_index:
     except Exception as e:
         print(f"Error during verification: {e}")
         return False
+
+def create_redeem_script(pubkeys, m: int):
+    m = format(80 + m, 'x')
+    n = format(80 + len(pubkeys), 'x')
+    print('m', m)
+    print('n', n)
+    script = m
+    for pubkey in pubkeys:
+        print(pubkey)
+        script += '21' + pubkey 
+    script += n + 'ae'
+    return script
+    
+def get_locking_bytecode(redeem_script) -> str:
+    '''
+    Returns the locking bytecode as hex string
+    '''
+    return Hash160().QuickDigest(bytes.fromhex(redeem_script)).hex()
+
+def get_locking_script(locking_bytecode: str):
+    return 'a9' + format(len(bytes.fromhex(locking_bytecode)), 'x') + locking_bytecode + '87'
+
+
+def get_multisig_wallet_locking_script(template: dict, locking_data: dict):
+    m = int(template['scripts']['lock']['script'].split('\n')[0].split('_')[1])
+    address_index = locking_data['hdKeys']['addressIndex'] or 0
+    xpubs = locking_data['hdKeys']['hdPublicKeys']
+    sorted_xpubs = [v for k, v in sorted(xpubs.items(), key=lambda item: int(item[0].split('_')[1]))]
+    pubkeys = [derive_pubkey_from_xpub(xpub, address_index) for xpub in sorted_xpubs]
+    redeem_script = create_redeem_script(pubkeys, m)
+    locking_bytecode = get_locking_bytecode(redeem_script)
+    locking_script = get_locking_script(locking_bytecode)
+
+    return locking_script
