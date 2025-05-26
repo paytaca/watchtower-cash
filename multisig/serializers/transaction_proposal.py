3ab14d1dbae804812b95a26a76c4d5cef012df16
ubuntu <rongrammer@hotmail.com>
2025-05-26 16:09:34 +0800

    fix tx proposal/sigs serializer

--- multisig/serializers/transaction_proposal.py
+++ multisig/serializers/transaction_proposal.py
@@ -1,41 +1,53 @@
+import logging
+from django.db import transaction
 from rest_framework import serializers
 from .wallet import MultisigWalletSerializer, SignerSerializer
 from ..models.transaction_proposal import MultisigTransactionProposal, Signature
+from ..models.wallet import Signer
+LOGGER = logging.getLogger(__name__)
 
 class SignatureSerializer(serializers.ModelSerializer):
     signer = serializers.PrimaryKeyRelatedField(read_only=True)
     transaction_proposal = serializers.PrimaryKeyRelatedField(read_only=True)
-    
+    inputIndex = serializers.IntegerField(source='input_index')
+    sigKey = serializers.CharField(source='signature_key')
+    sigValue = serializers.CharField(source='signature_value')
+
     class Meta:
         model = Signature
-        fields = ['id', 'signer', 'transaction_proposal', 'input_index', 'signature_key', 'signature_value']
-        read_only_fields = ['signers', 'created_at']
+        fields = ['id', 'signer', 'transaction_proposal', 'inputIndex', 'sigKey', 'sigValue']
+        read_only_fields = ['id', 'transaction_proposal']
 
 class MultisigTransactionProposalSerializer(serializers.ModelSerializer):
     wallet = serializers.PrimaryKeyRelatedField(read_only=True)
-    signatures = SignatureSerializer(many=True, read_only=True)
-    
+    signatures = SignatureSerializer(many=True, read_only=False)
+    sourceOutputs = serializers.JSONField(source='source_outputs')
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
-        signatures = {}
-        for sig in instance.signatures.all():
-            input_index = sig.input_index
-            if input_index not in signatures:
-                signatures[input_index] = {}
-            signatures[input_index][sig.signature_key] = sig.signature_value
-        
         request = self.context.get('request')
         if request:
             params = {k.lower(): v for k, v in request.query_params.items()}
             if params.get('expand_wallet') in ["1", "true", "yes", "on"]:
                 rep['wallet'] = MultisigWalletSerializer(instance.wallet, context=self.context).data
-        
-        rep['signatures'] = signatures
-        rep['sourceOutputs'] = rep.pop('source_outputs')
-
         return rep
+
+    def create(self, validated_data):
+        signatures_data = validated_data.pop('signatures', [])
+        with transaction.atomic():
+            proposal = MultisigTransactionProposal.objects.create(**validated_data)
+                        
+            for sig_data in signatures_data:
+                for entity in proposal.wallet.template['entities'].items():
+                    variable = sig_data['signature_key'].split('.')[0]
+                    signer, signer_entity_data = entity
+                    if signer_entity_data['variables'].get(variable):
+                        signer = Signer.objects.get(wallet=proposal.wallet, entity_key=signer)
+                        Signature.objects.create(transaction_proposal=proposal, signer=signer, **sig_data)
+
+        return proposal
+
 
     class Meta:
         model = MultisigTransactionProposal
-        fields = ['id', 'wallet', 'transaction', 'source_outputs', 'metadata', 'created_at', 'signatures']
+        fields = ['id', 'wallet', 'transaction', 'sourceOutputs', 'metadata', 'created_at', 'signatures']
