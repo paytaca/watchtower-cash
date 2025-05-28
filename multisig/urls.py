d4aaf34669b93140d1a6b7c6707a344253203e3e
ubuntu <rongrammer@hotmail.com>
2025-05-29 01:49:45 +0800

    add filter by locking bytecode and address index

--- multisig/urls.py
+++ multisig/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('wallets/<str:identifier>/', MultisigWalletDetailView.as_view(), name='wallet_detail') ,
     path('wallets/<int:pk>/rename/', RenameMultisigWalletView.as_view(), name='wallet-rename'),
     path('wallets/<int:pk>/delete/', MultisigWalletDeleteAPIView.as_view(), name='wallet-delete'),
-    path('wallets/<int:wallet_pk>/transaction-proposals/', MultisigTransactionProposalListCreateView.as_view(), name='transaction-proposal-list-create'),
+    path('wallets/<str:wallet_identifier>/transaction-proposals/', MultisigTransactionProposalListCreateView.as_view(), name='transaction-proposal-list-create'),
     path('wallets/<int:wallet_pk>/transaction-proposals/<int:proposal_id>/', MultisigTransactionProposalDetailView.as_view(), name='transaction-proposal-detail'),
     path('wallets/<int:wallet_pk>/transaction-proposals/<int:proposal_id>/signatures/<int:signer_id>', SignatureAddView.as_view(), name='transaction-proposal-signature-add'),
 ]
