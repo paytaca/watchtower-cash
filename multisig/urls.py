698fc49705755aa1c97c3d363debd5ebd15a8ee6
ubuntu <rongrammer@hotmail.com>
2025-06-09 17:50:45 +0800

    new migration

--- multisig/urls.py
+++ multisig/urls.py
@@ -17,7 +17,6 @@ urlpatterns = [
     path('wallets/<int:pk>/delete/', MultisigWalletDeleteAPIView.as_view(), name='wallet-delete'),
     path('wallets/<str:wallet_identifier>/transaction-proposals/', MultisigTransactionProposalListCreateView.as_view(), name='transaction-proposal-list-create'),
     path('transaction-proposals/<str:proposal_identifier>/', MultisigTransactionProposalDetailView.as_view(), name='transaction-proposal-detail'),
-    path('transaction-proposals/<str:proposal_identifier>/', MultisigTransactionProposalDetailView.as_view(), name='transaction-proposal-detail'),
     path('transaction-proposals/<str:proposal_identifier>/signatures/', SignaturesAddView.as_view(), name='transaction-proposal-signatures-add'),
     path('transaction-proposals/<str:proposal_identifier>/signatures/<str:signer_identifier>', SignerSignaturesAddView.as_view(), name='transaction-proposal-signer-signatures-add')
 ]
