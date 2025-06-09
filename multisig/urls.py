4b74b5264e65e433c29e32304918134588098216
ubuntu <rongrammer@hotmail.com>
2025-06-09 10:46:47 +0800

    multi type url params, accept id and other identifier

--- multisig/urls.py
+++ multisig/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('wallets/<str:identifier>/', MultisigWalletDetailView.as_view(), name='wallet_detail') ,
     path('wallets/<int:pk>/rename/', RenameMultisigWalletView.as_view(), name='wallet-rename'),
     path('wallets/<int:pk>/delete/', MultisigWalletDeleteAPIView.as_view(), name='wallet-delete'),
-    path('transaction-proposals/', MultisigTransactionProposalListCreateView.as_view(), name='transaction-proposal-list-create'),
+    path('wallets/<str:wallet_identifier>/transaction-proposals/', MultisigTransactionProposalListCreateView.as_view(), name='transaction-proposal-list-create'),
     path('transaction-proposals/<str:proposal_identifier>/', MultisigTransactionProposalDetailView.as_view(), name='transaction-proposal-detail'),
-    path('transaction-proposals/<str:proposal_identifier>/signatures/<str:signer_entity_key>', SignatureAddView.as_view(), name='transaction-proposal-signature-add'),
+    path('transaction-proposals/<str:proposal_identifier>/signatures/<str:signer_identifier>', SignatureAddView.as_view(), name='transaction-proposal-signature-add'),
 ]
