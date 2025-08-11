7eb7a7bc549114dc2d552999ae58ce4eac9bd9ab
SeanNikki123 <sslino@up.edu.ph>
2025-08-11 14:39:24 +0800

    added note and txid to memo model | added memo to admin

--- memos/admin.py
+++ memos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    Memo
+)
+
+
+@admin.register(Memo)
+class ShiftAdmin(admin.ModelAdmin):    
+    list_display = [
+        "wallet_hash",
+        "created_at",
+        "note",
+        "txid"
+    ]
--- memos/models.py
+++ memos/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Memo(models.Model):        
     wallet_hash = models.CharField(max_length=75, unique=True, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    instructions = models.TextField(blank=True, null=True)
+    note = models.TextField(blank=True, null=True)
+    txid = models.CharField(max_length=200, unique=True, null=True, db_index=True)
