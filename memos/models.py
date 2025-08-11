6b4f28cce768994ffb13e16b8281a42b543b9b17
SeanNikki123 <sslino@up.edu.ph>
2025-08-11 11:07:19 +0800

    added memo model

--- memos/models.py
+++ memos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Memo(models.Model):        
+    wallet_hash = models.CharField(max_length=75, unique=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    instructions = models.TextField(blank=True, null=True)
