bc356f32271bdd7b762ac510e96e052dbfd923e2
SeanNikki123 <sslino@up.edu.ph>
2025-08-12 18:00:55 +0800

    WIP add API for fetching memo

--- memos/views.py
+++ memos/views.py
@@ -18,22 +18,40 @@ from .serializers import (
 )
 
 class MemoView(APIView):
+	serializer_class = MemoSerializer
 
-    def post(self, request):
-    	data = request.data
-    	logger.info('Memos Post View: ')
-    	logger.info(data)
+	def post(self, request):
+		data = request.data
+		logger.info('Memos Post View: ')
+		logger.info(data)
 
-    	wallet_hash = request.headers.get('wallet_hash')
-    	#stage info
-    	info = {
-    		'wallet_hash': wallet_hash,
-    		'txid': data['txid'],
-    		'note': data['note']
-    	}
+		wallet_hash = request.headers.get('wallet_hash')
 
-    	serializer = MemoSerializer(data=info)
-    	serializer.is_valid(raise_exception=True)
-    	serializer.save()
+		#stage info
+		info = {
+			'wallet_hash': wallet_hash,
+			'txid': data['txid'],
+			'note': data['note']
+		}
 
-    	return Response(serializer.data, status=200)
+		serializer = MemoSerializer(data=info)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+
+		return Response(serializer.data, status=200)
+
+
+	def get(self, request):
+		wallet_hash = request.headers.get('wallet_hash')
+		txid = request.query_params.get('txid')
+
+		logger.info('Memos Get View: ')
+		logger.info(txid)
+
+		Model = self.serializer_class.Meta.model
+		data = {}
+
+		qs = Model.objects.filter(wallet_hash=wallet_hash, txid=txid)
+		logger.info(qs)
+
+		return Response({'success': True}, status=200)
