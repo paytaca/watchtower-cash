00a5784b7c18c43bfd2b99b3315406fd7017116a
SeanNikki123 <sslino@up.edu.ph>
2025-09-02 13:27:30 +0800

    set txid is non unique for sender/receiver to create their own memo with the same transaction

--- memos/models.py
+++ memos/models.py
@@ -5,4 +5,4 @@ class Memo(models.Model):
     wallet_hash = models.CharField(max_length=75, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     note = models.TextField(blank=True, null=True)
-    txid = models.CharField(max_length=200, unique=True, null=True, db_index=True)
+    txid = models.CharField(max_length=200, null=True, db_index=True)
