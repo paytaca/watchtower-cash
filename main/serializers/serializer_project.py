4c6a5906b3df6247cd8f6702a71c9f6076da875f
Jetszxcki <skyjeth1371120@gmail.com>
2025-10-09 11:16:41 +0800

    Change derivation of vault transaction stats count

--- main/serializers/serializer_project.py
+++ main/serializers/serializer_project.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 from main.models import Project, WalletHistory, TransactionMetaAttribute
 
 
-def get_transactions_by_attributes(project, attribute_value):
+def get_transactions_by_attributes(project_id, attribute_value):
+    project = Project.objects.get(id=project_id)
     wallet_hashes = project.wallets.values('wallet_hash').distinct('wallet_hash')
     meta_attributes = TransactionMetaAttribute.objects.filter(wallet_hash__in=wallet_hashes)
     meta_attributes = meta_attributes.filter(value=attribute_value)
@@ -44,17 +45,19 @@ class ProjectSerializer(serializers.ModelSerializer):
             '526ed3fd-8ce0-4960-8c2a-3faca848d180',
             '74fa1369-0a50-4baf-be9e-f8da7fcda6f5',
         ]
-        if obj.id in purelypeer_project_ids:
-            cashdrop_collect_txns = get_transactions_by_attributes(obj, 'Collected CashDrop BCH')
-            quest_nft_collect_txns = get_transactions_by_attributes(obj, 'Collected CashDrop NFT')
-            quest_payment_txns = get_transactions_by_attributes(obj, 'Quest Payment')
-            vault_txns = transactions.filter(address__address__istartswith='bitcoincash:p')
+        if str(obj.id) in purelypeer_project_ids:
+            cashdrop_collect_txns = get_transactions_by_attributes(obj.id, 'Collected CashDrop BCH')
+            quest_nft_collect_txns = get_transactions_by_attributes(obj.id, 'Collected CashDrop NFT')
+            quest_payment_txns = get_transactions_by_attributes(obj.id, 'Quest Payment')
+            vault_collect_txns = get_transactions_by_attributes(obj.id, 'Collected Vault BCH')
+            vault_payment_txns = get_transactions_by_attributes(obj.id, 'Vault Payment')
+            
             other_txns_count = {
                 'cashdrop_collect': cashdrop_collect_txns.count(),
                 'quest_nft_collect': quest_nft_collect_txns.count(),
                 'quest_payment': quest_payment_txns,
-                'vault_payment': vault_txns.filter(record_type='incoming').count(),
-                'vault_collect': vault_txns.filter(record_type='outgoing').count(),
+                'vault_collect': vault_collect_txns.count(),
+                'vault_payment': vault_payment_txns.count(),
             }
             txns_count = { **txns_count, **other_txns_count }
 
