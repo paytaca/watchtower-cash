59f418e66673b4dfa976afb7ab3b1f144dd37aa3
hantyrram <rongrammer@hotmail.com>
2025-10-22 00:02:32 +0800

    add 'networks' field

--- multisig/models/wallet.py
+++ multisig/models/wallet.py
@@ -16,16 +16,18 @@ class MultisigWallet(models.Model):
     deleted_at = models.DateTimeField(null=True, blank=True, default=None)
     updated_at = models.DateTimeField(auto_now=True)
     version = models.PositiveIntegerField(null=True, blank=True, default=0)
-    created_by = models.ForeignKey('Signer', on_delete=models.SET_NULL, null=True, blank=True)
+    created_by = models.ForeignKey("Signer", on_delete=models.SET_NULL, null=True, blank=True)
 
     name = models.CharField(max_length=250, default="Unnamed Multisig Wallet")
     m = models.PositiveSmallIntegerField()
     n = models.PositiveSmallIntegerField(null=True, blank=True)
     wallet_hash = models.CharField(max_length=200, help_text="The sha256 hash of the address 0 locking bytecode (The first set of lexicographically sorted public keys)")
+    
+    networks = JSONField(default=dict)
 
     @property
     def required_signatures(self):
-        # m = int(self.template.get('scripts')['lock']['script'].split('\n')[0].split('_')[1])
+        # m = int(self.template.get("scripts")["lock"]["script"].split("\n")[0].split("_")[1])
         # return m
         pass
 
@@ -35,9 +37,11 @@ class MultisigWallet(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
 
 class Signer(models.Model):
-    wallet = models.ForeignKey(MultisigWallet, related_name='signers', on_delete=models.CASCADE)
+    wallet = models.ForeignKey(MultisigWallet, related_name="signers", on_delete=models.CASCADE)
     entity_key = models.CharField(max_length=255, help_text="The signer's entity-key, Example: signer_1", null=True, blank=True)
     xpub = models.CharField(max_length=512, db_index=True, help_text="The xpub owned by the signer")
     name = models.CharField(max_length=200, help_text="Signers name")
@@ -45,7 +49,7 @@ class Signer(models.Model):
 
     class Meta:
         constraints = [
-                models.UniqueConstraint(fields=['wallet', 'xpub'], name='unique_signer')
+                models.UniqueConstraint(fields=["wallet", "xpub"], name="unique_signer")
             ]
     def __str__(self):
         return f"{self.entity_key}: {self.xpub}"
