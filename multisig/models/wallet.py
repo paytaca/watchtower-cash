e347e84fe5941b0996268df1efd4a8abcf711188
hantyrram <rongrammer@hotmail.com>
2026-01-19 17:37:10 +0800

    add coordinator field, add unique signer constraint

--- multisig/models/wallet.py
+++ multisig/models/wallet.py
@@ -1,5 +1,6 @@
 import logging
 from django.db import models
+from django.db.models import Q
 from django.utils import timezone
 
 LOGGER = logging.getLogger(__name__)
@@ -23,18 +24,18 @@ class MultisigWallet(models.Model):
 
 class Signer(models.Model):
     wallet = models.ForeignKey(MultisigWallet, related_name='signers', on_delete=models.CASCADE)
+    wallet_bsms_descriptor = models.TextField(help_text="The bsms descriptor of the wallet curated for this signer", null=True, blank=True)
     name = models.CharField(max_length=255, help_text="Name of the signer", null=True, blank=True)
-    pubkey_zero = models.CharField(max_length=66, help_text="The public key at address index 0", null=True, blank=True)
-
+    pubkey_zero = models.CharField(max_length=66, help_text="The public key of the signer's xpub at address index 0", null=True, blank=True)
+    coordinator = models.BooleanField(default=False, help_text="True if the signer is the coordinator", null=True, blank=True)
+    
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['wallet', 'pubkey_zero'], name='unique_signer')
+            models.UniqueConstraint(fields=['wallet', 'pubkey_zero'], name='unique_signer'),
+            models.UniqueConstraint(fields=['wallet'], condition=Q(coordinator=True), name='unique_coordinator_per_wallet')
         ]
 
     def __str__(self):
         return f"{self.name}: {self.pubkey_zero}"
 
 
-class MultisigWalletDescriptor(models.Model):
-    signer = models.ForeignKey(Signer, related_name='wallet_descriptors', on_delete=models.CASCADE)
-    descriptor = models.TextField(help_text="The bsms descriptor")
