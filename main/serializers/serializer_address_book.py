9e18b985e92532784d0716d3c1eafd8b0462f300
LukeHeimen <kdlanante@up.edu.ph>
2026-02-09 13:26:50 +0800

    Add AddressBookCreateSerializer and AddressBookAddressCreateSerializer; update AddressBookViewSet to handle creation of address books and associated addresses

--- main/serializers/serializer_address_book.py
+++ main/serializers/serializer_address_book.py
@@ -1,20 +1,26 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
-from main.models import AddressBook, AddressBookAddress
+from main.models import AddressBook, AddressBookAddress, Wallet
 
-class AddressBookListSerializer(ModelSerializer):
-    address_count = SerializerMethodField()
 
-    def get_address_count(self, obj):
-        return obj.address_book_addresses.count()
-
+class AddressBookAddressCreateSerializer(ModelSerializer):
+    address_book_id = serializers.IntegerField(write_only=True, required=True)
     class Meta:
-        model = AddressBook
+        model = AddressBookAddress
         fields = [
-            'id',
-            'name',
-            'is_favorite',
-            'address_count'
+            'address',
+            'address_type',
+            'address_book_id'
         ]
+
+    def create(self, validated_data):
+        address_book_id = validated_data.pop('address_book_id', None)
+        try:
+            address_book = AddressBook.objects.get(id=address_book_id)
+        except AddressBook.DoesNotExist:
+            raise serializers.ValidationError({'address_book_id': 'Address book not found.'})
+        validated_data['address_book'] = address_book
+        return super().create(validated_data)
 
 class AddressBookAddressSerializer(ModelSerializer):
     class Meta:
@@ -39,6 +45,37 @@ class AddressBookRetrieveSerializer(ModelSerializer):
             'created_at',
             'addresses'
         ]
+
+class AddressBookListSerializer(ModelSerializer):
+    address_count = SerializerMethodField()
+
+    def get_address_count(self, obj):
+        return obj.address_book_addresses.count()
+
+    class Meta:
+        model = AddressBook
+        fields = [
+            'id',
+            'name',
+            'is_favorite',
+            'address_count'
+        ]
+
+class AddressBookCreateSerializer(ModelSerializer):
+    wallet_hash = serializers.CharField(max_length=70, write_only=True, required=True)
+
+    class Meta:
+        model = AddressBook
+        fields = ['id', 'name', 'is_favorite', 'wallet_hash']
+
+    def create(self, validated_data):
+        wallet_hash = validated_data.pop('wallet_hash', None)
+        try:
+            wallet = Wallet.objects.get(wallet_hash=wallet_hash)
+        except Wallet.DoesNotExist:
+            raise serializers.ValidationError({'wallet_hash': 'Wallet not found.'})
+        validated_data['wallet'] = wallet
+        return super().create(validated_data)
 
 class AddressBookSerializer(ModelSerializer):
     class Meta:
--- main/views/view_address_book.py
+++ main/views/view_address_book.py
@@ -5,8 +5,12 @@ from main.serializers import (
     AddressBookSerializer,
     AddressBookAddressSerializer,
     AddressBookListSerializer,
-    AddressBookRetrieveSerializer
+    AddressBookRetrieveSerializer,
+    AddressBookCreateSerializer,
+
+    AddressBookAddressCreateSerializer
 )
+
 
 class AddressBookViewSet(
     viewsets.GenericViewSet,
@@ -36,10 +40,19 @@ class AddressBookViewSet(
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        serializer = AddressBookCreateSerializer(data=request.data.get('address_book'))
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        addresses = request.data.get('addresses')
+        if addresses and isinstance(addresses, list) and len(addresses) > 0:
+            for address in addresses:
+                address['address_book_id'] = serializer.data['id']
+                addresses_serializer = AddressBookAddressCreateSerializer(data=address)
+                addresses_serializer.is_valid(raise_exception=True)
+                addresses_serializer.save()
+        
+        return Response(serializer.data['id'], status=status.HTTP_201_CREATED)
 
     def update(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
