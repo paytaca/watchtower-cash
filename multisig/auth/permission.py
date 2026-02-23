ea7bff38309439f3840357369a6cbae498b5b2f2
hantyrram <rongrammer@hotmail.com>
2026-02-23 18:00:04 +0800

    update IsCosigner permission class

--- multisig/auth/permission.py
+++ multisig/auth/permission.py
@@ -1,61 +1,63 @@
 import logging
+import hashlib
 from multisig.auth.auth import parse_x_signature_header
 from multisig.js_client import verify_signature
 from multisig.utils import derive_pubkey_from_xpub
+from multisig.models.wallet import Signer
 from rest_framework import permissions
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from django.conf import settings
 
 LOGGER = logging.getLogger(__name__)
 
 class IsCosigner(permissions.BasePermission):
-    
-    def has_permission(self, request, view):
-        if getattr(settings, 'MULTISIG', {}).get('ENABLE_AUTH', False) == False:
-            return True
-        allow = False
-        if request.user and hasattr(request.user, 'signer'):
-            allow = True
-        if len((view.kwargs or {}).keys()) == 0 and request.method == 'GET': # not accessing specific resource
-            allow = True
-        return allow
-    
-class IsCosignerOfNewMultisigWallet(permissions.BasePermission):
+    """
+    Permission class that verifies a cosigner using their auth public key.
+
+    Expected headers:
+        - X-Auth-Cosigner-Auth-Public-Key: The raw public key derived at path 999/0
+        - X-Auth-Cosigner-Auth-Message: The message that was signed
+        - X-Auth-Cosigner-Auth-Signature: The signature in format 'schnorr=<sig>;der=<sig>'
+
+    The cosigner_auth_public_key is double-SHA256 hashed and compared against
+    the stored hash in the Signer model to identify the cosigner.
+
+    On successful authentication, attaches the matched Signer to request.signer.
+    """
 
     def has_permission(self, request, view):
-        if getattr(settings, 'MULTISIG', {}).get('ENABLE_AUTH', False) == False:
+        if not getattr(settings, "MULTISIG", {}).get("ENABLE_AUTH", False):
             return True
-        
-        message = request.headers.get('X-Auth-Message', '')
-        public_key = request.headers.get('X-Auth-PubKey', '')
-        signature = request.headers.get('X-Auth-Signature', '')
 
-        if not signature or not message or not public_key:
+        wallet_id = request.data.get("wallet")
+        if not wallet_id:
+            return False
+
+        auth_public_key = request.headers.get("X-Auth-Cosigner-Auth-PubKey", "")
+        message = request.headers.get("X-Auth-Cosigner-Auth-Message", "")
+        signature = request.headers.get("X-Auth-Cosigner-Auth-Signature", "")
+
+        if not auth_public_key or not message or not signature:
             return False
         
-        multisig_wallet = request.data
-        
-        try:
-            hd_public_keys = multisig_wallet['lockingData']['hdKeys']['hdPublicKeys']
-        except KeyError as ae:
-            LOGGER.info(ae)
-            raise ValidationError('Malformed multisig wallet lockingData')
-        
-        auth_credential_public_key_is_cosigner = False
+        signer = (
+            Signer.objects.filter(
+                wallet_id=wallet_id, auth_public_key=auth_public_key
+            )
+            .first()
+        )
 
-        for signer_entity_key in hd_public_keys.keys():
-            derived_public_key = derive_pubkey_from_xpub(hd_public_keys[signer_entity_key], 0)
-            if public_key == derived_public_key:
-                auth_credential_public_key_is_cosigner = True
-                break
-        
-        if not auth_credential_public_key_is_cosigner:
+        if not signer:
             return False
-        
+
         signature = parse_x_signature_header(signature)
-        sig_verification_response = verify_signature(message, public_key, signature)
+        sig_verification_response = verify_signature(
+            message, auth_public_key, signature
+        )
         sig_verification_result = sig_verification_response.json()
-        if sig_verification_result['success']:
+
+        if sig_verification_result.get("success", False):
+            request.signer = signer
             return True
-        
+
         return False
