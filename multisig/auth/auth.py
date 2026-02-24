3aae7901fb98b88fa0d0fc3a685468283b24c43c
hantyrram <rongrammer@hotmail.com>
2026-02-24 21:26:28 +0800

    use server identity as custom authenticated user

--- multisig/auth/auth.py
+++ multisig/auth/auth.py
@@ -1,6 +1,7 @@
 import time
 import logging
 from django.conf import settings
+from multisig.models.auth import ServerIdentity
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.authentication import BaseAuthentication
 from django.conf import settings
@@ -199,64 +200,13 @@ class PubKeySignatureMessageAuthentication(BaseAuthentication):
         sig_verification_result = sig_verification_response.json()
 
         if not sig_verification_result['success']:
+            
             raise AuthenticationFailed('Invalid signature')
-        return None
 
-# class MultisigAuthentication(BaseAuthentication):
-    
-#     def authenticate(self, request):
-    
-#         if not request.path.startswith('/api/multisig'):
-#             return None
-        
-#         message = request.headers.get('X-Auth-Message', '')
-        
-#         if not message or message.split(':')[0] != 'multisig':
-#             raise AuthenticationFailed('Invalid credential, invalid value for message')
+        identity = ServerIdentity.objects.get(public_key=public_key)
+        if not identity:
+            return (None, None)
 
-#         if getattr(settings, 'MULTISIG', {}).get('ENABLE_AUTH', False) == False:
-#             return (None, None)
+        auth = { 'public_key': public_key, 'message': message, 'signature': signature, 'sig_verification_result': sig_verification_result }
 
-#         public_key = request.headers.get('X-Auth-PubKey', '')
-#         signature = request.headers.get('X-Auth-Signature', '')
-#         if not public_key or not signature:
-#             raise AuthenticationFailed('Invalid credentials, missing public key or signature')
-
-#         if not hasattr(request, 'resolver_match') or request.resolver_match == None:
-#             request.resolver_match = resolve(request.path_info)
-
-#         wallet_identifier = request.resolver_match.kwargs.get('wallet_identifier')
-#         proposal_identifier = request.resolver_match.kwargs.get('proposal_identifier')  
-        
-#         wallet = None 
-#         signer = None
-
-#         if wallet_identifier:
-#             if wallet_identifier.isdigit():
-#                 wallet = MultisigWallet.objects.prefetch_related('signers').filter(id = int(wallet_identifier)).first()
-#             else:
-#                 wallet = MultisigWallet.objects.prefetch_related('signers').filter(locking_bytecode=wallet_identifier).first()
-#         elif proposal_identifier:
-#             if proposal_identifier.isdigit():
-#                 wallet = MultisigWallet.objects.prefetch_related('signers').filter(multisigtransactionproposal__id=int(proposal_identifier)).first()
-#             else:
-#                 wallet = MultisigWallet.objects.prefetch_related('signers').filter(multisigtransactionproposal__locking_bytecode=proposal_identifier).first()        
-        
-#         user = MultisigStatelessUser(wallet=wallet)
-
-
-#         if wallet and public_key:
-#             for signer in wallet.signers.all():
-#                 derived_public_key = derive_pubkey_from_xpub(signer.xpub, 0)
-#                 if public_key == derived_public_key:
-#                     user.signer = signer
-#                     break 
-        
-#         user.wallet = wallet
-#         user.auth_data = {
-#             'public_key': public_key,
-#             'message': message,
-#             'signature': parse_x_signature_header(signature)
-#         }
-#         return (user, user.auth_data)
-        
+        return (identity, auth)
